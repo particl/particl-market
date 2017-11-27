946d46cf0d93be3573c0c3e8757ef92d81cc6c6b
Juha Kovanen <juha.kovanen@gmail.com>
2017-11-27 02:28:57 +0200

    itemimagedata it, added some validations

--- src/api/services/ItemImageDataService.ts
+++ src/api/services/ItemImageDataService.ts
@@ -4,6 +4,7 @@ import { Logger as LoggerType } from '../../core/Logger';
 import { Types, Core, Targets } from '../../constants';
 import { validate, request } from '../../core/api/Validate';
 import { NotFoundException } from '../exceptions/NotFoundException';
+import { ValidationException } from '../exceptions/ValidationException';
 import { ItemImageDataRepository } from '../repositories/ItemImageDataRepository';
 import { ItemImageData } from '../models/ItemImageData';
 import { ItemImageDataCreateRequest } from '../requests/ItemImageDataCreateRequest';
@@ -22,18 +23,8 @@ export class ItemImageDataService {
         this.log = new Logger(__filename);
     }
 
-    @validate()
-    public async rpcFindAll( @request(RpcRequest) data: any): Promise<Bookshelf.Collection<ItemImageData>> {
-        return this.findAll();
-    }
-
     public async findAll(): Promise<Bookshelf.Collection<ItemImageData>> {
         return this.itemImageDataRepo.findAll();
-    }
-
-    @validate()
-    public async rpcFindOne( @request(RpcRequest) data: any): Promise<ItemImageData> {
-        return this.findOne(data.params[0]);
     }
 
     public async findOne(id: number, withRelated: boolean = true): Promise<ItemImageData> {
@@ -46,18 +37,13 @@ export class ItemImageDataService {
     }
 
     @validate()
-    public async rpcCreate( @request(RpcRequest) data: any): Promise<ItemImageData> {
-        return this.create({
-            dataId: data.params[0] || '',
-            protocol: data.params[1] || '',
-            encoding: data.params[2] || '',
-            data: data.params[3] || ''
-        });
-    }
-
-    @validate()
     public async create( @request(ItemImageDataCreateRequest) body: any): Promise<ItemImageData> {
 
+        // todo: could this be annotated in ItemImageDataCreateRequest?
+        // todo: improve validation
+        if (body.dataId == null && body.protocol == null && body.encoding == null && body.data == null ) {
+            throw new ValidationException('Request body is not valid', ['dataId, protocol, encoding and data cannot all be null']);
+        }
         // If the request body was valid we will create the itemImageData
         const itemImageData = await this.itemImageDataRepo.create(body);
 
@@ -67,17 +53,13 @@ export class ItemImageDataService {
     }
 
     @validate()
-    public async rpcUpdate( @request(RpcRequest) data: any): Promise<ItemImageData> {
-        return this.update(data.params[0], {
-            dataId: data.params[1] || '',
-            protocol: data.params[2] || '',
-            encoding: data.params[3] || '',
-            data: data.params[4] || ''
-        });
-    }
-
-    @validate()
     public async update(id: number, @request(ItemImageDataUpdateRequest) body: any): Promise<ItemImageData> {
+
+        // todo: could this be annotated in ItemImageDataUpdateRequest?
+        // todo: improve validation
+        if (body.dataId == null && body.protocol == null && body.encoding == null && body.data == null ) {
+            throw new ValidationException('Request body is not valid', ['dataId, protocol, encoding and data cannot all be null']);
+        }
 
         // find the existing one without related
         const itemImageData = await this.findOne(id, false);
@@ -93,13 +75,44 @@ export class ItemImageDataService {
         return updatedItemImageData;
     }
 
+    public async destroy(id: number): Promise<void> {
+        await this.itemImageDataRepo.destroy(id);
+    }
+
+    // TODO: REMOVE
+    @validate()
+    public async rpcFindAll( @request(RpcRequest) data: any): Promise<Bookshelf.Collection<ItemImageData>> {
+        return this.findAll();
+    }
+
+    @validate()
+    public async rpcFindOne( @request(RpcRequest) data: any): Promise<ItemImageData> {
+        return this.findOne(data.params[0]);
+    }
+
+    @validate()
+    public async rpcCreate( @request(RpcRequest) data: any): Promise<ItemImageData> {
+        return this.create({
+            dataId: data.params[0] || '',
+            protocol: data.params[1] || '',
+            encoding: data.params[2] || '',
+            data: data.params[3] || ''
+        });
+    }
+
+    @validate()
+    public async rpcUpdate( @request(RpcRequest) data: any): Promise<ItemImageData> {
+        return this.update(data.params[0], {
+            dataId: data.params[1] || '',
+            protocol: data.params[2] || '',
+            encoding: data.params[3] || '',
+            data: data.params[4] || ''
+        });
+    }
+
     @validate()
     public async rpcDestroy( @request(RpcRequest) data: any): Promise<void> {
         return this.destroy(data.params[0]);
-    }
-
-    public async destroy(id: number): Promise<void> {
-        await this.itemImageDataRepo.destroy(id);
     }
 
 }
---
+++ test/integration/ItemImageData.test.ts
@@ -0,0 +1,113 @@
+import { app } from '../../src/app';
+import { Logger as LoggerType } from '../../src/core/Logger';
+import { Types, Core, Targets } from '../../src/constants';
+import { TestUtil } from './lib/TestUtil';
+import { TestDataService } from '../../src/api/services/TestDataService';
+
+import { ValidationException } from '../../src/api/exceptions/ValidationException';
+import { NotFoundException } from '../../src/api/exceptions/NotFoundException';
+
+import { ItemImageData } from '../../src/api/models/ItemImageData';
+import { ImageDataProtocolType } from '../../src/api/enums/ImageDataProtocolType';
+
+import { ItemImageDataService } from '../../src/api/services/ItemImageDataService';
+
+describe('ItemImageData', () => {
+
+    const log: LoggerType = new LoggerType(__filename);
+    const testUtil = new TestUtil();
+
+    let testDataService: TestDataService;
+    let itemImageDataService: ItemImageDataService;
+
+    let createdId;
+
+    const testData = {
+        dataId: 'QmUwHMFY9GSiKgjqyZpgAv2LhBrh7GV8rtLuagbry9wmMU',
+        protocol: ImageDataProtocolType.IPFS,
+        encoding: null,
+        data: null
+    };
+
+    const testDataUpdated = {
+        dataId: null,
+        protocol: ImageDataProtocolType.LOCAL,
+        encoding: 'BASE64',
+        data: 'BASE64 encoded image data'
+    };
+
+    beforeAll(async () => {
+        await testUtil.bootstrapAppContainer(app);  // bootstrap the app
+
+        testDataService = app.IoC.getNamed<TestDataService>(Types.Service, Targets.Service.TestDataService);
+        itemImageDataService = app.IoC.getNamed<ItemImageDataService>(Types.Service, Targets.Service.ItemImageDataService);
+
+        // clean up the db, first removes all data and then seeds the db with default data
+        await testDataService.clean([]);
+    });
+
+    afterAll(async () => {
+        //
+    });
+
+    test('Should create a new item image data', async () => {
+        const itemImageDataModel: ItemImageData = await itemImageDataService.create(testData);
+        createdId = itemImageDataModel.Id;
+
+        const result = itemImageDataModel.toJSON();
+
+        expect(result.dataId).toBe(testData.dataId);
+        expect(result.protocol).toBe(testData.protocol);
+        expect(result.encoding).toBe(testData.encoding);
+        expect(result.data).toBe(testData.data);
+    });
+
+    test('Should throw ValidationException because we want to create a empty item image data', async () => {
+        expect.assertions(1);
+        await itemImageDataService.create({}).catch(e =>
+            expect(e).toEqual(new ValidationException('Request body is not valid', []))
+        );
+    });
+
+    test('Should list item image datas with our new create one', async () => {
+        const itemImageDataCollection = await itemImageDataService.findAll();
+        const itemImageData = itemImageDataCollection.toJSON();
+        expect(itemImageData.length).toBe(1);
+
+        const result = itemImageData[0];
+
+        expect(result.dataId).toBe(testData.dataId);
+        expect(result.protocol).toBe(testData.protocol);
+        expect(result.encoding).toBe(testData.encoding);
+        expect(result.data).toBe(testData.data);
+    });
+
+    test('Should return one item image data', async () => {
+        const itemImageDataModel: ItemImageData = await itemImageDataService.findOne(createdId);
+        const result = itemImageDataModel.toJSON();
+
+        expect(result.dataId).toBe(testData.dataId);
+        expect(result.protocol).toBe(testData.protocol);
+        expect(result.encoding).toBe(testData.encoding);
+        expect(result.data).toBe(testData.data);
+    });
+
+    test('Should update the item image data', async () => {
+        const itemImageDataModel: ItemImageData = await itemImageDataService.update(createdId, testDataUpdated);
+        const result = itemImageDataModel.toJSON();
+
+        expect(result.dataId).toBe(testDataUpdated.dataId);
+        expect(result.protocol).toBe(testDataUpdated.protocol);
+        expect(result.encoding).toBe(testDataUpdated.encoding);
+        expect(result.data).toBe(testDataUpdated.data);
+    });
+
+    test('Should delete the item image data', async () => {
+        expect.assertions(1);
+        await itemImageDataService.destroy(createdId);
+        await itemImageDataService.findOne(createdId).catch(e =>
+            expect(e).toEqual(new NotFoundException(createdId))
+        );
+    });
+
+});
