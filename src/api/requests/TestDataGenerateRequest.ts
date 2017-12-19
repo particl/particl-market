ab0b09497eb0a5ac9bc92bffd8361c58182aa8a7
Juha Kovanen <juha.kovanen@gmail.com>
2017-12-19 16:29:29 +0200

    TestDataGenerateRequest

--- src/api/requests/TestDataGenerateRequest.ts
+++ src/api/requests/TestDataGenerateRequest.ts
@@ -2,13 +2,13 @@ import { IsNotEmpty } from 'class-validator';
 import { RequestBody } from '../../core/api/RequestBody';
 
 // tslint:disable:variable-name
-export class TestDataCreateRequest extends RequestBody {
+export class TestDataGenerateRequest extends RequestBody {
 
     @IsNotEmpty()
     public model: string;
 
     @IsNotEmpty()
-    public data: string;
+    public amount: string;
 
     @IsNotEmpty()
     public withRelated: boolean;
