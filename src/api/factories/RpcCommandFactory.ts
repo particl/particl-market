00cb5dfa018f09962b3e975afc51cc141f347c8d
Suyash-cis <suyash.n@cisinlabs.com>
2017-12-20 20:33:26 +0530

    Rpc commands for the accept, reject and cancel bid

--- src/api/factories/RpcCommandFactory.ts
+++ src/api/factories/RpcCommandFactory.ts
@@ -50,6 +50,11 @@ import { ProfileGetCommand } from '../commands/profile/ProfileGetCommand';
 import { ShippingDestinationAddCommand } from '../commands/shippingdestination/ShippingDestinationAddCommand';
 import { ShippingDestinationRemoveCommand } from '../commands/shippingdestination/ShippingDestinationRemoveCommand';
 
+import { AcceptBidCommand } from '../commands/bid/AcceptBidCommand';
+import { CancelBidCommand } from '../commands/bid/CancelBidCommand';
+import { RejectBidCommand } from '../commands/bid/RejectBidCommand';
+import { SendBidCommand } from '../commands/bid/SendBidCommand';
+
 // tslint:disable:array-type
 // tslint:disable:max-line-length
 export class RpcCommandFactory {
@@ -103,6 +108,10 @@ export class RpcCommandFactory {
         @inject(Types.Command) @named(Targets.Command.CleanDbCommand) private cleanDbCommand: CleanDbCommand,
         @inject(Types.Command) @named(Targets.Command.GenerateDataCommand) private generateDataCommand: GenerateDataCommand,
         @inject(Types.Command) @named(Targets.Command.HelpCommand) private helpCommand: HelpCommand,
+        @inject(Types.Command) @named(Targets.Command.bid.AcceptBidCommand) private acceptBidCommand: AcceptBidCommand,
+        @inject(Types.Command) @named(Targets.Command.bid.CancelBidCommand) private cancelBidCommand: CancelBidCommand,
+        @inject(Types.Command) @named(Targets.Command.bid.RejectBidCommand) private rejectBidCommand: RejectBidCommand,
+        @inject(Types.Command) @named(Targets.Command.bid.SendBidCommand) private sendBidCommand: SendBidCommand,
 
         //  ---
         // @multiInject(Types.Command) public commands: RpcCommand<any>[],
@@ -156,6 +165,10 @@ export class RpcCommandFactory {
         this.commands.push(cleanDbCommand);
         this.commands.push(generateDataCommand);
         this.commands.push(helpCommand);
+        this.commands.push(acceptBidCommand);
+        this.commands.push(cancelBidCommand);
+        this.commands.push(rejectBidCommand);
+        this.commands.push(sendBidCommand);
 
         this.log.debug(this.commands.length + 'commands initialized.');
 
