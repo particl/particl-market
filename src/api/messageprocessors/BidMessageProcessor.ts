58037302d4d9b8f48c6ca4ba068f050070e4c842
Juha Kovanen <juha.kovanen@gmail.com>
2018-01-02 15:34:19 +0200

    fixed messageinterfaces and escrowservice

--- src/api/messageprocessors/BidMessageProcessor.ts
+++ src/api/messageprocessors/BidMessageProcessor.ts
@@ -32,10 +32,10 @@ export class BidMessageProcessor implements MessageProcessorInterface {
             this.log.warn(`ListingItem with the hash=${message.item} was not found!`);
             throw new NotFoundException(message.item);
         } else {
-            const bid = this.bidFactory.get(message);
+            // const bid = this.bidFactory.get(message);
             // setting the bid relation with listingItem
-            bid['listing_item_id'] = listingItem.id;
-            return await this.bidService.create(bid);
+            // bid['listing_item_id'] = listingItem.id;
+            // return await this.bidService.create(bid);
         }
     }
 }
