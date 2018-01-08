b0c3d38324c939f8c1d3f8aa493291d5b2dbaacd
tP6SnbnsW <a1209491@student.adelaide.edu.au>
2018-01-08 22:40:54 +1030

    Test that objects with the matching values match hashes

--- test/unit/core/ObjectHash.test.ts
+++ test/unit/core/ObjectHash.test.ts
@@ -47,12 +47,39 @@ describe('ObjectHash', () => {
         ]
     };
 
+    const testCategory3reordered2 = {
+        Key: 'cat_ROOT',
+        id: 1,
+        name: 'ROOT',
+        ChildItemCategories: [
+            {
+                Key: 'cat_high_value',
+                ChildItemCategories: [
+                    {
+                        id: 3,
+                        Key: 'cat_high_business_corporate',
+                        name: 'Something else',
+                        parent_item_category_id: 2,
+                        ChildItemCategories: []
+                    }
+                ],
+                name: 'High Value',
+                parent_item_category_id: 1,
+                id: 2
+            }
+        ]
+    };
+
     test('getHash() should return correct hash', () => {
         expect(ObjectHash.getHash(testCategory)).toBe('639bf0f3e0f70e6d948d2715bbce1cb0232e0667e82639c31a183efa29a73397');
     });
 
     test('getHash() should return different hashes for objects that have different values', () => {
         expect(ObjectHash.getHash(testCategory)).not.toBe(ObjectHash.getHash(testCategory2));
+    });
+
+    test('getHash() should return same hashes for objects that have same values in different orders', () => {
+        expect(ObjectHash.getHash(testCategory2)).toBe(ObjectHash.getHash(testCategory3reordered2));
     });
 
     test('getHash() on an object should return the same hash every time', () => {
