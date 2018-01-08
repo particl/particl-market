0dd1315f854064d14aefb18ce141ba21ca5cbe9d
Suyash-cis <suyash.n@cisinlabs.com>
2018-01-08 16:05:49 +0530

    update the bidSearchParams validation for action

--- src/api/requests/BidSearchParams.ts
+++ src/api/requests/BidSearchParams.ts
@@ -1,12 +1,12 @@
-import { IsEnum, IsNotEmpty } from 'class-validator';
+import { ValidateIf, IsEnum, IsNotEmpty } from 'class-validator';
 import { RequestBody } from '../../core/api/RequestBody';
 import { BidMessageType } from '../enums/BidMessageType';
 
 // tslint:disable:variable-name
 export class BidSearchParams extends RequestBody {
 
-    // TODO: For now I have commented this code need to discuss
-    // @IsEnum(BidMessageType)
+    @ValidateIf(o => o.action)
+    @IsEnum(BidMessageType)
     public action: BidMessageType;
 
     public listingItemId: number;
