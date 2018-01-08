1e68c06d76095bb936c6eb8421c2998fdb197546
tP6SnbnsW <a1209491@student.adelaide.edu.au>
2018-01-08 22:03:27 +1030

    Fixed hash function

--- src/core/helpers/ObjectHash.ts
+++ src/core/helpers/ObjectHash.ts
@@ -2,6 +2,6 @@ import * as crypto from 'crypto-js';
 
 export class ObjectHash {
     public static getHash(obj: any): string {
-        return crypto.SHA256(JSON.stringify(obj)).toString();
+        return crypto.SHA256(JSON.stringify(obj).split('').sort().toString()).toString();
     }
 }
