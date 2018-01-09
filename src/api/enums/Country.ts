b841a40cc93e8b1d819effbbf4858bb99e69b4e0
tP6SnbnsW <a1209491@student.adelaide.edu.au>
2018-01-10 00:23:15 +1030

    Put country back in for temporary backwards compatibility

--- src/api/enums/Country.ts
+++ src/api/enums/Country.ts
@@ -31,3 +31,13 @@ export class ShippingCountries {
     }
 }
 ShippingCountries.initialize();
+
+export enum Country {
+    EU = 'EU',
+    USA = 'USA',
+    ASIA = 'ASIA',
+    FINLAND = 'FINLAND',
+    SWEDEN = 'SWEDEN',
+    SOUTH_AFRICA = 'SOUTH_AFRICA',
+    UNITED_KINGDOM = 'UNITED_KINGDOM'
+}
