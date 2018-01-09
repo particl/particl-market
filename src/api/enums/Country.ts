34d57ec0cc4fef4c80ef7b0df76df603202d93eb
tP6SnbnsW <a1209491@student.adelaide.edu.au>
2018-01-10 00:45:45 +1030

    Moved shipping countries into its own file

--- src/api/enums/Country.ts
+++ src/api/enums/Country.ts
@@ -1,37 +1,3 @@
-import { getDataSet, reduce } from 'iso3166-2-db';
-
-export class ShippingCountries {
-    public static countryCodeList;
-    public static countryList;
-
-    public static initialize(): void {
-        this.countryCodeList = reduce(getDataSet(), 'en');
-        this.countryList = {};
-        for ( const x in this.countryCodeList ) {
-            if ( x ) {
-                this.countryList[this.countryCodeList[x].name] = x;
-            }
-        }
-    }
-
-    public static isValidCountry( country: string ): boolean {
-        if ( this.countryList[country] ) {
-            return true;
-        } else {
-            return false;
-        }
-    }
-
-    public static isValidCountryCode( countryCode: string ): boolean {
-        if ( this.countryCodeList[countryCode] ) {
-            return true;
-        } else {
-            return false;
-        }
-    }
-}
-ShippingCountries.initialize();
-
 export enum Country {
     EU = 'EU',
     USA = 'USA',
