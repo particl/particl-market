1d557be71e50ff79e866e144815e3f4cb603fce5
navjot.g <navjot.g@cisinlabs.com>
2018-01-09 21:03:53 +0530

    CryptocurrencyAddress :: TEST CASES UPDATION

--- src/api/requests/CryptocurrencyAddressCreateRequest.ts
+++ src/api/requests/CryptocurrencyAddressCreateRequest.ts
@@ -1,13 +1,15 @@
-import { IsNotEmpty } from 'class-validator';
+import { IsNotEmpty, IsEnum } from 'class-validator';
 import { RequestBody } from '../../core/api/RequestBody';
+import { CryptocurrencyAddressType } from '../enums/CryptocurrencyAddressType';
 
 // tslint:disable:variable-name
 export class CryptocurrencyAddressCreateRequest extends RequestBody {
 
     public profile_id: number;
 
+    @IsEnum(CryptocurrencyAddressType)
     @IsNotEmpty()
-    public type: string;
+    public type: CryptocurrencyAddressType;
 
     @IsNotEmpty()
     public address: string;
