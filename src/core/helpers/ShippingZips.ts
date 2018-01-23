7697cc70bcd79f45570e69b594d015f8a4503fc3
tP6SnbnsW <a1209491@student.adelaide.edu.au>
2018-01-24 03:55:14 +1030

    ZIP code validation and tests of validation (incomplete)

--- src/core/helpers/ShippingZips.ts
+++ src/core/helpers/ShippingZips.ts
@@ -5,12 +5,68 @@ import zipit = require('zip-it');
 
 export class ShippingZips {
     public static isCountryCodeSupported( countryCode: string ): boolean {
-        return zipit.isCountrySupported(countryCode.toUpperCase());
+        countryCode = countryCode.toUpperCase();
+        if ( zipit.isCountrySupported(countryCode) ) {
+            return true;
+        }
+        //Countries not supported by the lib but should be.
+        switch ( countryCode ) {
+            case 'AQ':
+            case 'GS':
+            case 'IO':
+            case 'NI':
+            case 'PN':
+            case 'TF':
+            case 'UM':
+                return true;
+            default:
+                return false;
+        }
     }
 
     /*
      */
     public static validate( countryCode: string, zip: string ): boolean {
-        return zipit.verifyCode(countryCode.toUpperCase(), zip);
+        countryCode = countryCode.toUpperCase();
+        zip = zip.toString().toUpperCase();
+        {
+            let retval;
+            if ( retval = zipit.verifyCode(countryCode, zip) ) {
+                return retval;
+            }
+        }
+
+        switch ( countryCode ) {
+            case 'AQ':
+            case 'IO':
+            case 'GS':
+            case 'PN':
+            case 'TF':
+                // No postal code registered.
+                // https://en.wikipedia.org/wiki/ISO_3166-2:AQ
+                // https://en.wikipedia.org/wiki/ISO_3166-2:IO
+                // https://en.wikipedia.org/wiki/ISO_3166-2:GS
+                // https://en.wikipedia.org/wiki/ISO_3166-2:PN
+                // https://en.wikipedia.org/wiki/ISO_3166-2:TF
+                return true;
+            case 'NI': {
+                // https://en.wikipedia.org/wiki/ISO_3166-2:NI
+                const regex = new RegExp('^[ \t]*NI[ \t-]*(BO|CA|CI|CO|ES|GR|JI|LE|MD|MN|MS|MT|NS|SJ|RI|AN|AS)[ \t]*$');
+                if ( regex.test(zip) ) {
+                    return true;
+                }
+                return false;
+            }
+            case 'UM': {
+                // https://en.wikipedia.org/wiki/ISO_3166-2:UM
+                const regex = new RegExp('^[ \t]*UM[ \t-]*(81|84|86|67|89|71|76|95|79)[ \t]*$');
+                if ( regex.test(zip) ) {
+                    return true;
+                }
+                return false;
+            }
+            default:
+                return false;
+        }
     }
 }
