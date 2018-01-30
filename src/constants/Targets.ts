7c06e1bc93b3da6d4c4d8655981d4d7770d0b9e3
tP6SnbnsW <a1209491@student.adelaide.edu.au>
2018-01-30 19:31:05 +1030

    Shipping destination commands refactoring and implementing

--- src/constants/Targets.ts
+++ src/constants/Targets.ts
@@ -136,13 +136,13 @@ export const Targets = {
         },
         HelpCommand: 'HelpCommand',
         itemcategory: {
-            ItemCategoryListCommand: 'ItemCategoryListCommand',
             ItemCategoryCreateCommand: 'ItemCategoryCreateCommand',
             ItemCategoryFindCommand: 'ItemCategoryFindCommand',
             ItemCategoryGetCommand: 'ItemCategoryGetCommand',
+            ItemCategoryListCommand: 'ItemCategoryListCommand',
             ItemCategoryRemoveCommand: 'ItemCategoryRemoveCommand',
-            ItemCategoryUpdateCommand: 'ItemCategoryUpdateCommand',
-            ItemCategoryRootCommand: 'ItemCategoryRootCommand'
+            ItemCategoryRootCommand: 'ItemCategoryRootCommand',
+            ItemCategoryUpdateCommand: 'ItemCategoryUpdateCommand'
         },
         itemimage: {
             ItemImageAddCommand: 'ItemImageAddCommand',
@@ -161,21 +161,21 @@ export const Targets = {
         },
         listingitem: {
             ListingItemGetCommand: 'ListingItemGetCommand',
+            ListingItemRootCommand: 'ListingItemRootCommand',
             ListingItemSearchCommand: 'ListingItemSearchCommand',
-            ListingItemUpdateCommand : 'ListingItemUpdateCommand',
-            ListingItemRootCommand: 'ListingItemRootCommand'
+            ListingItemUpdateCommand: 'ListingItemUpdateCommand'
         },
         listingitemtemplate: {
             ListingItemTemplateCreateCommand: 'ListingItemTemplateCreateCommand',
             ListingItemTemplateDestroyCommand: 'ListingItemTemplateDestroyCommand',
             ListingItemTemplateGetCommand: 'ListingItemTemplateGetCommand',
-            ListingItemTemplateSearchCommand: 'ListingItemTemplateSearchCommand',
-            ListingItemTemplatePostCommand: 'ListingItemTemplatePostCommand'
+            ListingItemTemplatePostCommand: 'ListingItemTemplatePostCommand',
+            ListingItemTemplateSearchCommand: 'ListingItemTemplateSearchCommand'
         },
         market: {
             MarketCreateCommand: 'MarketCreateCommand',
-            MarketRootCommand: 'MarketRootCommand',
-            MarketListCommand: 'MarketListCommand'
+            MarketListCommand: 'MarketListCommand',
+            MarketRootCommand: 'MarketRootCommand'
         },
         messaginginformation: {
             MessagingInformationUpdateCommand: 'MessagingInformationUpdateCommand'
@@ -187,14 +187,16 @@ export const Targets = {
             ProfileCreateCommand: 'ProfileCreateCommand',
             ProfileDestroyCommand: 'ProfileDestroyCommand',
             ProfileGetCommand: 'ProfileGetCommand',
-            ProfileUpdateCommand: 'ProfileUpdateCommand',
+            ProfileListCommand: 'ProfileListCommand',
             ProfileRootCommand: 'ProfileRootCommand',
-            ProfileListCommand: 'ProfileListCommand'
+            ProfileUpdateCommand: 'ProfileUpdateCommand'
         },
         RpcCommandInterface: 'RpcCommandInterface',
         shippingdestination: {
             ShippingDestinationAddCommand: 'ShippingDestinationAddCommand',
-            ShippingDestinationRemoveCommand: 'ShippingDestinationRemoveCommand'
+            ShippingDestinationListCommand: 'ShippingDestinationListCommand',
+            ShippingDestinationRemoveCommand: 'ShippingDestinationRemoveCommand',
+            ShippingDestinationRootCommand: 'ShippingDestinationRootCommand'
         }
     },
     Factory:     {
@@ -210,11 +212,11 @@ export const Targets = {
         BidMessageProcessor: 'BidMessageProcessor',
         CancelBidMessageProcessor: 'CancelBidMessageProcessor',
         ListingItemMessageProcessor: 'ListingItemMessageProcessor',
-        UpdateListingItemMessageProcessor: 'UpdateListingItemMessageProcessor',
         MessageProcessor: 'MessageProcessor',
         MessageProcessorInterface: 'MessageProcessorInterface',
         RejectBidMessageProcessor: 'RejectBidMessageProcessor',
-        TestMessageProcessor: 'TestMessageProcessor'
+        TestMessageProcessor: 'TestMessageProcessor',
+        UpdateListingItemMessageProcessor: 'UpdateListingItemMessageProcessor'
     },
     Middleware:     {
         AuthenticateMiddleware: 'AuthenticateMiddleware',
