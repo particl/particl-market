cf6e51793a60b5c2000ed67cc4e1f8f0b195e0b4
Juha Kovanen <juha.kovanen@gmail.com>
2018-01-30 14:59:30 +0200

    added imageversion to imagedata so we can have multiple versions of one image

---
+++ src/api/enums/ImageVersion.ts
@@ -0,0 +1,13 @@
+/**
+ * ImageVersionEnum
+ *
+ */
+
+export enum ImageVersion {
+
+    ORIGINAL = 'ORIGINAL',      // originally received
+    THUMBNAIL = 'THUMBNAIL',    // locally cached version
+    MEDIUM = 'MEDIUM',          // locally cached version
+    LARGE = 'LARGE'             // locally cached version
+
+}
