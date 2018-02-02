ba104de159b5d5912531dfc7c607455d6d6deab4
Juha Kovanen <juha.kovanen@gmail.com>
2018-02-02 13:09:08 +0200

    build.json

--- bin/ci-create-build-version.sh
+++ bin/ci-create-build-version.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 VERSION="$(git log -n 1 --date=short --format=format:"rev.%ad.%h" HEAD)"
-echo "{\n\t\"version\": \"$VERSION\"\n}" > public/cli/build.json
+echo "{\"version\": \"$VERSION\"}" > public/cli/build.json
