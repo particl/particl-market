5a2af7febeba906a238eea41ee15e1d404495463
Suyash-cis <suyash.n@cisinlabs.com>
2018-02-05 11:39:51 +0530

    removed trailing whitespace

--- test/black-box/RejectBidCommand.test.ts
+++ test/black-box/RejectBidCommand.test.ts
@@ -10,7 +10,7 @@ describe('RejectBidCommand', () => {
     const bidFactory = null;
     const listingItemService = null;
     const messageBroadcastService = null;
-    
+
     const method =  Commands.BID_ROOT.commandName;
     const subMethod =  Commands.BID_REJECT.commandName;
 
