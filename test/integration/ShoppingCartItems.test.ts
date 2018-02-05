4d7c0861c429c888d506ecb099e435b0df93c012
navjot.g <navjot.g@cisinlabs.com>
2018-02-05 13:41:44 +0530

    integration test shoppingCartItems

--- test/integration/ShoppingCartItems.test.ts
+++ test/integration/ShoppingCartItems.test.ts
@@ -8,8 +8,17 @@ import { ValidationException } from '../../src/api/exceptions/ValidationExceptio
 import { NotFoundException } from '../../src/api/exceptions/NotFoundException';
 
 import { ShoppingCartItems } from '../../src/api/models/ShoppingCartItems';
+import { ListingItem } from '../../src/api/models/ListingItem';
 
+import { MarketService } from '../../src/api/services/MarketService';
+import { ProfileService } from '../../src/api/services/ProfileService';
 import { ShoppingCartItemsService } from '../../src/api/services/ShoppingCartItemsService';
+import { ListingItemService } from '../../src/api/services/ListingItemService';
+
+import { ShoppingCartItemsCreateRequest } from '../../src/api/requests/ShoppingCartItemsCreateRequest';
+
+import { TestDataCreateRequest } from '../../src/api/requests/TestDataCreateRequest';
+import { reset } from 'chalk';
 
 describe('ShoppingCartItems', () => {
     jasmine.DEFAULT_TIMEOUT_INTERVAL = process.env.JASMINE_TIMEOUT;
@@ -17,48 +26,65 @@ describe('ShoppingCartItems', () => {
     const testUtil = new TestUtil();
 
     let testDataService: TestDataService;
+    let marketService: MarketService;
+    let profileService: ProfileService;
     let shoppingCartItemsService: ShoppingCartItemsService;
+    let listingItemService: ListingItemService;
 
     let createdId;
+    let createdListingItem;
+    let defaultShoppingCart;
 
     const testData = {
-    };
+        shopping_cart_id: 0,
+        listing_item_id: 0
+    } as ShoppingCartItemsCreateRequest;
 
-    const testDataUpdated = {
-    };
+    // const testDataUpdated = {
+    // };
 
     beforeAll(async () => {
         await testUtil.bootstrapAppContainer(app);  // bootstrap the app
 
         testDataService = app.IoC.getNamed<TestDataService>(Types.Service, Targets.Service.TestDataService);
+        marketService = app.IoC.getNamed<MarketService>(Types.Service, Targets.Service.MarketService);
+        profileService = app.IoC.getNamed<ProfileService>(Types.Service, Targets.Service.ProfileService);
         shoppingCartItemsService = app.IoC.getNamed<ShoppingCartItemsService>(Types.Service, Targets.Service.ShoppingCartItemsService);
+        listingItemService = app.IoC.getNamed<ListingItemService>(Types.Service, Targets.Service.ListingItemService);
 
         // clean up the db, first removes all data and then seeds the db with default data
         await testDataService.clean([]);
+        // listingItem
+        let defaultMarket = await marketService.getDefault();
+        defaultMarket = defaultMarket.toJSON();
+        const defaultProfile = await profileService.getDefault();
+        defaultShoppingCart = defaultProfile.toJSON().ShoppingCarts[0];
+        createdListingItem = await testDataService.create<ListingItem>({
+            model: 'listingitem',
+            data: {
+                market_id: defaultMarket.id,
+                hash: 'itemhash'
+            } as any,
+            withRelated: true
+        } as TestDataCreateRequest);
     });
 
     afterAll(async () => {
         //
     });
 
-    /*
-    test('Should throw ValidationException because there is no related_id', async () => {
-        expect.assertions(1);
-        await shoppingCartItemsService.create(testData).catch(e =>
-            expect(e).toEqual(new ValidationException('Request body is not valid', []))
-        );
-    });
-    */
-
     test('Should create a new shopping cart items', async () => {
-        // testData['related_id'] = 0;
+        testData.shopping_cart_id = defaultShoppingCart.id;
+        testData.listing_item_id = createdListingItem.id;
         const shoppingCartItemsModel: ShoppingCartItems = await shoppingCartItemsService.create(testData);
         createdId = shoppingCartItemsModel.Id;
 
         const result = shoppingCartItemsModel.toJSON();
 
         // test the values
-        // expect(result.value).toBe(testData.value);
+        expect(result.id).not.toBeUndefined();
+        expect(result.shoppingCartId).toBe(testData.shopping_cart_id);
+        expect(result.listingItemId).toBe(testData.listing_item_id);
     });
 
     test('Should throw ValidationException because we want to create a empty shopping cart items', async () => {
@@ -76,7 +102,9 @@ describe('ShoppingCartItems', () => {
         const result = shoppingCartItems[0];
 
         // test the values
-        // expect(result.value).toBe(testData.value);
+
+        expect(result.shoppingCartId).toBe(testData.shopping_cart_id);
+        expect(result.listingItemId).toBe(testData.listing_item_id);
     });
 
     test('Should return one shopping cart items', async () => {
@@ -84,32 +112,38 @@ describe('ShoppingCartItems', () => {
         const result = shoppingCartItemsModel.toJSON();
 
         // test the values
-        // expect(result.value).toBe(testData.value);
+        expect(result.shoppingCartId).toBe(testData.shopping_cart_id);
+        expect(result.listingItemId).toBe(testData.listing_item_id);
     });
 
-    /*
-    test('Should throw ValidationException because there is no related_id', async () => {
-        expect.assertions(1);
-        await shoppingCartItemsService.update(createdId, testDataUpdated).catch(e =>
-            expect(e).toEqual(new ValidationException('Request body is not valid', []))
-        );
-    });
-    */
+    // /*
+    // test('Should throw ValidationException because there is no related_id', async () => {
+    //     expect.assertions(1);
+    //     await shoppingCartItemsService.update(createdId, testDataUpdated).catch(e =>
+    //         expect(e).toEqual(new ValidationException('Request body is not valid', []))
+    //     );
+    // });
+    // */
 
-    test('Should update the shopping cart items', async () => {
-        // testDataUpdated['related_id'] = 0;
-        const shoppingCartItemsModel: ShoppingCartItems = await shoppingCartItemsService.update(createdId, testDataUpdated);
-        const result = shoppingCartItemsModel.toJSON();
+    // test('Should update the shopping cart items', async () => {
+    //     // testDataUpdated['related_id'] = 0;
+    //     const shoppingCartItemsModel: ShoppingCartItems = await shoppingCartItemsService.update(createdId, testDataUpdated);
+    //     const result = shoppingCartItemsModel.toJSON();
 
-        // test the values
-        // expect(result.value).toBe(testDataUpdated.value);
-    });
+    //     // test the values
+    //     // expect(result.value).toBe(testDataUpdated.value);
+    // });
 
     test('Should delete the shopping cart items', async () => {
-        expect.assertions(1);
+        expect.assertions(2);
         await shoppingCartItemsService.destroy(createdId);
         await shoppingCartItemsService.findOne(createdId).catch(e =>
             expect(e).toEqual(new NotFoundException(createdId))
+        );
+
+        await listingItemService.destroy(createdListingItem.id);
+        await listingItemService.findOne(createdListingItem.id).catch(e =>
+            expect(e).toEqual(new NotFoundException(createdListingItem.id))
         );
     });
 
