fdc09ecf5ac031e8aa21aeebb9b4fbf2950c5766
Juha Kovanen <juha.kovanen@gmail.com>
2018-02-09 17:27:23 +0200

    asdfghjklöäå

--- bin/ci-create-dbs.sh
+++ bin/ci-create-dbs.sh
@@ -2,6 +2,8 @@
 rm -rf data/marketplace*db
 rm -rf data/app1/marketplace*db
 rm -rf data/app2/marketplace*db
+mkdir -p data/app1
+mkdir -p data/app2
 npm run db:migrate
 cp data/marketplace.db data/marketplace-test.db
 cp data/marketplace.db data/app1/marketplace.db
