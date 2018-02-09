c87196e5d2ac0d7110f184137385be6877725b92
Juha Kovanen <juha.kovanen@gmail.com>
2018-02-09 13:50:45 +0200

    travis build test3

--- bin/ci-blackbox-tests.sh
+++ bin/ci-blackbox-tests.sh
@@ -1,3 +1,11 @@
 #!/bin/sh
+export NODE_ENV=$1
+export APP_PORT=$2
+export APP_HOST=$3
+export RPCUSER=$4
+export RPCPASSWORD=$5
+export RPCHOSTNAME=$6
+export MAINNET_PORT=$7
+export TESTNET_PORT=$8
 wait-port localhost:3100
 npm run test:black-box:pretty
