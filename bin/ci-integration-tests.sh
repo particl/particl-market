d946eba1f80520743479fff71ee708f59e00e4f2
Juha Kovanen <juha.kovanen@gmail.com>
2018-02-09 13:59:13 +0200

    wait-port

--- bin/ci-integration-tests.sh
+++ bin/ci-integration-tests.sh
@@ -1,3 +1,11 @@
 #!/bin/sh
-wait-port localhost:3100
+export NODE_ENV=$1
+export APP_PORT=$2
+export APP_HOST=$3
+export RPCUSER=$4
+export RPCPASSWORD=$5
+export RPCHOSTNAME=$6
+export MAINNET_PORT=$7
+export TESTNET_PORT=$8
+wait-port $APP_HOST:$APP_PORT/cli
 npm run test:integration:pretty
