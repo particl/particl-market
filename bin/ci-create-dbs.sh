b13538b40de24d4c365203d0e525951daa03cb98
Juha Kovanen <juha.kovanen@gmail.com>
2018-02-09 17:23:37 +0200

    asdfghjklöä

--- bin/ci-create-dbs.sh
+++ bin/ci-create-dbs.sh
@@ -1,4 +1,10 @@
 #!/bin/sh
 rm -rf data/marketplace*db
+rm -rf data/app1/marketplace*db
+rm -rf data/app2/marketplace*db
 npm run db:migrate
 cp data/marketplace.db data/marketplace-test.db
+cp data/marketplace.db data/app1/marketplace.db
+cp data/marketplace.db data/app1/marketplace-test.db
+cp data/marketplace.db data/app2/marketplace.db
+cp data/marketplace.db data/app2/marketplace-test.db
--- bin/ci-entrypoint.sh
+++ bin/ci-entrypoint.sh
@@ -2,13 +2,12 @@
 
 #set -e
 ls -al data
-ls -al data/app1
-ls -al data/app2
 
 yarn install
-rm -rf data/marketplace.db
-rm -rf data/marketplace-test.db
-npm run db:migrate
-cp -rf data/marketplace.db data/marketplace-test.db
+# rm -rf data/marketplace.db
+# rm -rf data/marketplace-test.db
+# npm run db:migrate
+# cp -rf data/marketplace.db data/marketplace-test.db
+
 bin/ci-create-build-version.sh
 npm run serve
