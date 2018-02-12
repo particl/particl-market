88509fa135e3a316e99a8597033a9847cb217bf3
Juha Kovanen <juha.kovanen@gmail.com>
2018-02-12 18:32:49 +0200

    ci-entrypoint.sh

--- bin/ci-entrypoint.sh
+++ bin/ci-entrypoint.sh
@@ -1,13 +1,10 @@
 #!/bin/sh
 
 #set -e
-ls -al data
-
 yarn install
-# rm -rf data/marketplace.db
-# rm -rf data/marketplace-test.db
-# npm run db:migrate
-# cp -rf data/marketplace.db data/marketplace-test.db
-
+rm -rf data/marketplace.db
+rm -rf data/marketplace-test.db
+npm run db:migrate
+cp -rf data/marketplace.db data/marketplace-test.db
 bin/ci-create-build-version.sh
 npm run serve
