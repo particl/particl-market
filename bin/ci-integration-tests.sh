6d0cb76dbee0b5f99a91771349e9b9c7bd94d52b
Juha Kovanen <juha.kovanen@gmail.com>
2018-02-12 13:59:46 +0200

    test

--- bin/ci-integration-tests.sh
+++ bin/ci-integration-tests.sh
@@ -1,16 +1,2 @@
 #!/bin/sh
-export NODE_ENV=$1
-export APP_PORT=$2
-export APP_HOST=$3
-export RPCUSER=$4
-export RPCPASSWORD=$5
-export RPCHOSTNAME=$6
-export MAINNET_PORT=$7
-export TESTNET_PORT=$8
-docker-compose logs --tail=10000
-cat .env
-cat test/.env.test
-cat test/.env.blackbox
-
-wait-port $APP_HOST:$APP_PORT/cli
 npm run test:integration:pretty
