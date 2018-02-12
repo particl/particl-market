8c3abdb9573f748e2696f2658969d91ecbb7b1f4
tP6SnbnsW <a1209491@student.adelaide.edu.au>
2018-02-13 00:03:25 +1030

    Help command mk2. Supports arbitary commands

--- src/api/commands/HelpCommand.ts
+++ src/api/commands/HelpCommand.ts
@@ -29,80 +29,85 @@ export class HelpCommand extends BaseCommand implements RpcCommandInterface<stri
      */
     @validate()
     public async execute( @request(RpcRequest) data: any, rpcCommandFactory: RpcCommandFactory): Promise<string> {
-        let helpStr = '';
-        if ( data.params.length < 1 ) {
+        let helpStr = this.generateHelp(data.params, rpcCommandFactory);
+        helpStr = helpStr.trim(); // Remove trailing \n
+        return helpStr;
+    }
+
+    public generateHelp( commands: string[], rpcCommandFactory: RpcCommandFactory ): string {
+        if ( commands.length <= 0 ) {
+            let helpStr = '';
             for ( const rootCommand of Commands.rootCommands ) {
                 if ( rootCommand ) {
                     let command;
                     try {
                         command = rpcCommandFactory.get(rootCommand);
+                        helpStr += command.help() + '\n';
                     } catch ( ex ) {
                         this.log.warn(`help(): Couldn't find ${rootCommand}.`);
                         continue;
                     }
-                    helpStr += command.help() + '\n';
                 }
             }
+            return helpStr;
         } else {
-            const rootCommandName = data.params[0];
-            let rootCommand;
-            let rootCommandCommand;
-            let foundRoot = false;
-            for ( rootCommand of Commands.rootCommands ) {
-                if ( rootCommand ) {
-                    if ( rootCommand.commandName === rootCommandName ) {
-                        foundRoot = true;
-                        rootCommandCommand = rpcCommandFactory.get(rootCommand);
-                        break;
+            const commandName = commands.shift();
+            try {
+                for ( const rootCommand of Commands.rootCommands ) {
+                    if ( rootCommand.commandName === commandName) {
+                        return this._generateHelp(commands, rpcCommandFactory, rootCommand);
                     }
                 }
-            }
-            if ( !foundRoot ) {
-                // Root command wasn't found
-                throw new NotFoundException(`Command <${data.params[0]}> not found.`); 
-            }
-
-            if ( data.params.length <= 1 ) {
-                /*
-                 * Get all children of the root command.
-                 */
-                helpStr = rootCommandCommand.help() + '\n';
-
-                for ( const childCommand of rootCommand.childCommands ) {
-                    let childCommandCommand;
-                    try {
-                        childCommandCommand = rpcCommandFactory.get(childCommand);
-                    } catch ( ex ) {
-                        this.log.warn(`help(): Couldn't find ${childCommand}.`);
-                        continue;
-                    }
-                    helpStr += childCommandCommand.help() + '\n';
-                }
-            } else { // if ( data.params.legnt <= 2 ) {
-                /*
-                 * Get just a single child of the root command.
-                 */
-                for ( const childCommand of rootCommand.childCommands ) {
-                    if ( childCommand.commandName === data.params[1] ) {
-                        let childCommandCommand;
-                        try {
-                            childCommandCommand = rpcCommandFactory.get(childCommand);
-                        } catch ( ex ) {
-                            this.log.warn(`help(): Couldn't find ${childCommand}.`);
-                            continue;
-                        }
-                        return  rootCommandName + ' ' + childCommandCommand.help();
-                    }
-                }
-                throw new NotFoundException(`Command <${data.params[0]} ${data.params[1]}> not found.`);
+            } catch ( ex ) {
+                throw new NotFoundException(`Command <${commandName}> not found.`);
             }
         }
-        helpStr = helpStr.trim(); // Remove trailing \n
-        return helpStr;
+        throw new NotFoundException(`Command not found.`);
+    }
+
+    public _generateHelp( commands: string[], rpcCommandFactory: RpcCommandFactory, command: any ): string {
+        if ( commands.length === 0 ) {
+            let retStr = '';
+
+            if ( command.childCommands.length > 0 ) {
+                // Get the help for every sub command and return it.
+                for ( const childCommand of command.childCommands ) {
+                    let commandCommand;
+                    try {
+                        commandCommand = rpcCommandFactory.get(childCommand);
+                        retStr += commandCommand.help() + '\n';
+                    } catch ( ex ) {
+                        this.log.warn(`Command <${command} ${childCommand}> not found.`);
+                        continue;
+                    }
+                }
+                return retStr;
+            } else {
+                // Just get the help for this command and return it.
+                let commandCommand;
+                try {
+                    commandCommand = rpcCommandFactory.get(command);
+                } catch ( ex ) {
+                    this.log.warn(`Command <${command}> not found.`);
+                    throw new NotFoundException(`Command <${command}> not found.`);
+                }
+                return commandCommand.help();
+            }
+        }
+
+        // Keep recursing down.
+        const commandName = commands.shift();
+        for ( const childCommand of command.childCommands ) {
+            if ( childCommand.commandName === commandName ) {
+                return this._generateHelp(commands, rpcCommandFactory, childCommand);
+            }
+        }
+        this.log.warn(`Command <${command}> not found.`);
+        throw new NotFoundException(`Command <${command}> not found.`);
     }
 
     public help(): string {
-        return this.getName() + ' [<command> [<subCommand>]] \n'
+        return this.getName() + ' [<command> [<subCommand> [...]]] \n'
             + '    <command>                - [optional] String - Command that we want to view help for. \n'
             + '    <subCommand>             - [optional] String - Subcommand that we want to view help for. ';
     }
