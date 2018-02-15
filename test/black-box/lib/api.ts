02f2e8942d196efbcbd8cd0f538cf20501a4f005
navjot.g <navjot.g@cisinlabs.com>
2018-02-15 14:02:27 +0530

    Add authentication on black-box test cases.

--- test/black-box/lib/api.ts
+++ test/black-box/lib/api.ts
@@ -1,5 +1,5 @@
 import * as dotenv from 'dotenv';
-dotenv.config({path: './test/.env.blackbox'});
+dotenv.config({ path: './test/.env.blackbox' });
 import * as _ from 'lodash';
 import * as request from 'request-promise';
 import { Options } from 'request-promise';
@@ -14,11 +14,12 @@ export interface ApiOptions<T> {
     port?: number;
 }
 
-export const api = async <T> (method: string, path: string, options: ApiOptions<T> = {}) => {
+export const api = async <T>(method: string, path: string, options: ApiOptions<T> = {}) => {
 
     const HOST = options.host ? options.host : process.env.APP_HOST;
     const PORT = options.port ? options.port : process.env.APP_PORT;
     const uri = `${HOST}:${PORT}${path}`;
+    const auth = 'Basic ' + new Buffer(process.env.USER_NAME + ':' + process.env.USER_PASSWORD).toString('base64');
 
     if (!_.has(options, 'headers')) {
         options.headers = {};
@@ -28,6 +29,8 @@ export const api = async <T> (method: string, path: string, options: ApiOptions<
         if (!_.has(options, 'Content-Type')) {
             options.headers['Content-Type'] = 'application/json';
         }
+        if (auth)
+            options.headers['Authorization'] = auth;
     }
 
     const o: Options = {
@@ -51,31 +54,31 @@ export const api = async <T> (method: string, path: string, options: ApiOptions<
     }
     return new ApiResponseTest(error, res);
 
-/*
-    await request(o)
-        .then(res => {
-            console.log('res:', res.body);
-            return new ApiResponseTest(null, res);
-        })
-        .catch(StatusCodeError, reason => {
-            console.log('error:', reason.error.statusCode + ': ' + reason.error.message);
-            return new ApiResponseTest(reason.error, null);
-        })
-        .catch(TransformError, reason => {
-            console.log(reason.cause.message); // => Transform failed!
-            return new ApiResponseTest(reason.error, null);
-        })
-        .catch(RequestError, reason => {
-            // The request failed due to technical reasons.
-            // reason.cause is the Error object Request would pass into a callback.
-            throw new HttpException(500, reason.error.message);
-        });
-*/
+    /*
+        await request(o)
+            .then(res => {
+                console.log('res:', res.body);
+                return new ApiResponseTest(null, res);
+            })
+            .catch(StatusCodeError, reason => {
+                console.log('error:', reason.error.statusCode + ': ' + reason.error.message);
+                return new ApiResponseTest(reason.error, null);
+            })
+            .catch(TransformError, reason => {
+                console.log(reason.cause.message); // => Transform failed!
+                return new ApiResponseTest(reason.error, null);
+            })
+            .catch(RequestError, reason => {
+                // The request failed due to technical reasons.
+                // reason.cause is the Error object Request would pass into a callback.
+                throw new HttpException(500, reason.error.message);
+            });
+    */
 
 };
 
 
-export const rpc = async (method: string, params: any[] = [] ): any => {
+export const rpc = async (method: string, params: any[] = []): any => {
     const body = { method, params, jsonrpc: '2.0' };
     return await api('POST', '/api/rpc', { body });
 };
