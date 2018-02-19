b57c774a6a705683c32e56833abb078b32b4a6d3
Juha Kovanen <juha.kovanen@gmail.com>
2018-02-19 13:09:57 +0200

    .

--- bin/ci-integration-tests.sh
+++ bin/ci-integration-tests.sh
@@ -1,2 +1,10 @@
 #!/bin/sh
+export NODE_ENV=$1
+export APP_PORT=$2
+export APP_HOST=$3
+export RPCUSER=$4
+export RPCPASSWORD=$5
+export RPCHOSTNAME=$6
+export MAINNET_PORT=$7
+export TESTNET_PORT=$8
 npm run test:integration:pretty
