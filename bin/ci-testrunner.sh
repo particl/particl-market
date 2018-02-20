2f48cc52ef1fcc2bd0e3e6f2711c8bf8ebe21848
Juha Kovanen <juha.kovanen@gmail.com>
2018-02-20 13:16:52 +0200

    .

--- bin/ci-testrunner.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-
-yarn install
-rm -rf data/marketplace.db
-rm -rf data/marketplace-test.db
-npm run db:migrate
-cp -rf data/marketplace.db data/marketplace-test.db
-bin/ci-create-build-version.sh
