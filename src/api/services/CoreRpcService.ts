1a8415e3e0564e7b4ae0fc714f6a6a33a90c6a04
navjot.g <navjot.g@cisinlabs.com>
2018-03-05 16:25:10 +0530

    Solved conflict, also correct member-ordering linting err

--- src/api/services/CoreRpcService.ts
+++ src/api/services/CoreRpcService.ts
@@ -103,6 +103,23 @@ export class CoreRpcService {
 
     }
 
+    public async getNewAddressFromDaemon(): Promise<string> {
+        let newAddress;
+        await this.call('getnewaddress')
+            .then( async (res) => {
+                this.log.info('Successfully created new address for profile: ' + res);
+                newAddress = res;
+            })
+            .catch(reason => {
+                this.log.warn('Could not create new address for profile: ' + reason);
+                newAddress = 'ERROR_NO_ADDRESS';
+            });
+        if ( newAddress ) {
+            return newAddress;
+        }
+        throw new Error('Something has gone terribly wrong.');
+    }
+
     private getOptions(): any {
 
         const auth = {
@@ -123,23 +140,6 @@ export class CoreRpcService {
 
         // this.log.debug('initializing rpc with opts:', rpcOpts);
         return rpcOpts;
-    }
-
-    public async getNewAddressFromDaemon(): Promise<string> {
-        let newAddress;
-        await this.call('getnewaddress')
-            .then( async (res) => {
-                this.log.info('Successfully created new address for profile: ' + res);
-                newAddress = res;
-            })
-            .catch(reason => {
-                this.log.warn('Could not create new address for profile: ' + reason);
-                newAddress = 'ERROR_NO_ADDRESS';
-            });
-        if ( newAddress ) {
-            return newAddress;
-        }
-        throw new Error('Something has gone terribly wrong.');
     }
 
     private getUrl(): string {
