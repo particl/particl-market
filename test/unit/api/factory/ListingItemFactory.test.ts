0ceafed11b9d2f94188a4ce10883d71807347a89
Juha Kovanen <juha.kovanen@gmail.com>
2018-03-07 18:05:50 +0200

    facotry tests

--- test/unit/api/factory/ListingItemFactory.test.ts
+++ test/unit/api/factory/ListingItemFactory.test.ts
@@ -7,8 +7,8 @@ import { ListingItemMessage } from '../../../../src/api/messages/ListingItemMess
 
 import { ObjectHash } from '../../../../src/core/helpers/ObjectHash';
 
-import * as listingItemTemplateTestData from '../../data/listingItemTemplate.json';
-import * as listingItemCategoryWithRelatedTestData from '../../data/listingItemCategoryWithRelated.json';
+import * as listingItemTemplateBasic from '../../testdata/listingItemTemplateBasic.json';
+import * as listingItemCategoryWithRelatedTestData from '../../testdata/listingItemCategoryWithRelated.json';
 
 describe('ListingItemFactory', () => {
 
@@ -31,20 +31,6 @@ describe('ListingItemFactory', () => {
         expect(message).not.toHaveProperty('ListingItemObjects');
         expect(message).not.toHaveProperty('Profile');
         expect(message).not.toHaveProperty('ListingItem');
-    };
-// tslint:disable:max-line-length
-    test('Should get ListingItemMessage', async () => {
-
-        const message: ListingItemMessage = await listingItemFactory
-            .getMessage(listingItemTemplateTestData, listingItemCategoryWithRelatedTestData);
-
-        console.log('message: ', JSON.stringify(message, null, 2));
-        // console.log('message.information: ', JSON.stringify(message.information, null, 2));
-        // console.log('message.information.id: ', message.information.id);
-        // console.log('message.ItemInformation: ', JSON.stringify(message.ItemInformation, null, 2));
-
-        // message
-        expectMessageFromListingItem(message, listingItemTemplateTestData);
 
         // message.information
         expect(message.information).toBeDefined();
@@ -59,9 +45,9 @@ describe('ListingItemFactory', () => {
         expect(message.information).not.toHaveProperty('ItemCategory');
         expect(message.information).not.toHaveProperty('ItemLocation');
         expect(message.information).not.toHaveProperty('ShippingDestinations');
-        expect(message.information.title).toBe(listingItemTemplateTestData.ItemInformation.title);
-        expect(message.information.short_description).toBe(listingItemTemplateTestData.ItemInformation.shortDescription);
-        expect(message.information.long_description).toBe(listingItemTemplateTestData.ItemInformation.longDescription);
+        expect(message.information.title).toBe(testData.ItemInformation.title);
+        expect(message.information.short_description).toBe(testData.ItemInformation.shortDescription);
+        expect(message.information.long_description).toBe(testData.ItemInformation.longDescription);
         expect(message.information.category).toBeDefined();
         expect(message.information.category.length).toBe(3);
         expect(message.information.category[0]).toBe('cat_ROOT');
@@ -75,8 +61,8 @@ describe('ListingItemFactory', () => {
         expect(message.information.location).not.toHaveProperty('updatedAt');
         expect(message.information.location).not.toHaveProperty('createdAt');
         expect(message.information.location).not.toHaveProperty('LocationMarker');
-        expect(message.information.location.country).toBe(listingItemTemplateTestData.ItemInformation.ItemLocation.region);
-        expect(message.information.location.address).toBe(listingItemTemplateTestData.ItemInformation.ItemLocation.address);
+        expect(message.information.location.country).toBe(testData.ItemInformation.ItemLocation.region);
+        expect(message.information.location.address).toBe(testData.ItemInformation.ItemLocation.address);
 
         // message.information.location.gps
         expect(message.information.location.gps).toBeDefined();
@@ -86,10 +72,10 @@ describe('ListingItemFactory', () => {
         expect(message.information.location.gps).not.toHaveProperty('createdAt');
         expect(message.information.location.gps).not.toHaveProperty('markerTitle');
         expect(message.information.location.gps).not.toHaveProperty('markerText');
-        expect(message.information.location.gps.marker_title).toBe(listingItemTemplateTestData.ItemInformation.ItemLocation.LocationMarker.markerTitle);
-        expect(message.information.location.gps.marker_text).toBe(listingItemTemplateTestData.ItemInformation.ItemLocation.LocationMarker.markerText);
-        expect(message.information.location.gps.lat).toBe(listingItemTemplateTestData.ItemInformation.ItemLocation.LocationMarker.lat);
-        expect(message.information.location.gps.lng).toBe(listingItemTemplateTestData.ItemInformation.ItemLocation.LocationMarker.lng);
+        expect(message.information.location.gps.marker_title).toBe(testData.ItemInformation.ItemLocation.LocationMarker.markerTitle);
+        expect(message.information.location.gps.marker_text).toBe(testData.ItemInformation.ItemLocation.LocationMarker.markerText);
+        expect(message.information.location.gps.lat).toBe(testData.ItemInformation.ItemLocation.LocationMarker.lat);
+        expect(message.information.location.gps.lng).toBe(testData.ItemInformation.ItemLocation.LocationMarker.lng);
 
         // message.information.shipping_destinations
         expect(message.information.shipping_destinations).toBeDefined();
@@ -105,10 +91,10 @@ describe('ListingItemFactory', () => {
         expect(message.information.images[0]).not.toHaveProperty('updatedAt');
         expect(message.information.images[0]).not.toHaveProperty('createdAt');
         expect(message.information.images[0]).not.toHaveProperty('ItemImageDatas');
-        expect(message.information.images[0].hash).toBe(listingItemTemplateTestData.ItemInformation.ItemImages[0].hash);
-        expect(message.information.images[0].data[0].protocol).toBe(listingItemTemplateTestData.ItemInformation.ItemImages[0].ItemImageDatas[0].protocol);
-        expect(message.information.images[0].data[0].encoding).toBe(listingItemTemplateTestData.ItemInformation.ItemImages[0].ItemImageDatas[0].encoding);
-        expect(message.information.images[0].data[0].data).toBe(listingItemTemplateTestData.ItemInformation.ItemImages[0].ItemImageDatas[0].data);
+        expect(message.information.images[0].hash).toBe(testData.ItemInformation.ItemImages[0].hash);
+        expect(message.information.images[0].data[0].protocol).toBe(testData.ItemInformation.ItemImages[0].ItemImageDatas[0].protocol);
+        expect(message.information.images[0].data[0].encoding).toBe(testData.ItemInformation.ItemImages[0].ItemImageDatas[0].encoding);
+        expect(message.information.images[0].data[0].data).toBe(testData.ItemInformation.ItemImages[0].ItemImageDatas[0].data);
 
         // message.payment
         expect(message.payment).toBeDefined();
@@ -119,7 +105,7 @@ describe('ListingItemFactory', () => {
         expect(message.payment).not.toHaveProperty('createdAt');
         expect(message.payment).not.toHaveProperty('ItemPrice');
         expect(message.payment).not.toHaveProperty('Escrow');
-        expect(message.payment.type).toBe(listingItemTemplateTestData.PaymentInformation.type);
+        expect(message.payment.type).toBe(testData.PaymentInformation.type);
 
         // message.payment.cryptocurrency
         expect(message.payment.cryptocurrency).toBeDefined();
@@ -132,8 +118,8 @@ describe('ListingItemFactory', () => {
         expect(message.payment.cryptocurrency[0]).not.toHaveProperty('createdAt');
         expect(message.payment.cryptocurrency[0]).not.toHaveProperty('CryptocurrencyAddress');
         expect(message.payment.cryptocurrency[0]).not.toHaveProperty('ShippingPrice');
-        expect(message.payment.cryptocurrency[0].currency).toBe(listingItemTemplateTestData.PaymentInformation.ItemPrice.currency);
-        expect(message.payment.cryptocurrency[0].base_price).toBe(listingItemTemplateTestData.PaymentInformation.ItemPrice.basePrice);
+        expect(message.payment.cryptocurrency[0].currency).toBe(testData.PaymentInformation.ItemPrice.currency);
+        expect(message.payment.cryptocurrency[0].base_price).toBe(testData.PaymentInformation.ItemPrice.basePrice);
 
         // message.payment.cryptocurrency.address
         expect(message.payment.cryptocurrency[0].address).toBeDefined();
@@ -141,8 +127,8 @@ describe('ListingItemFactory', () => {
         expect(message.payment.cryptocurrency[0].address).not.toHaveProperty('profileId');
         expect(message.payment.cryptocurrency[0].address).not.toHaveProperty('updatedAt');
         expect(message.payment.cryptocurrency[0].address).not.toHaveProperty('createdAt');
-        expect(message.payment.cryptocurrency[0].address.type).toBe(listingItemTemplateTestData.PaymentInformation.ItemPrice.CryptocurrencyAddress.type);
-        expect(message.payment.cryptocurrency[0].address.address).toBe(listingItemTemplateTestData.PaymentInformation.ItemPrice.CryptocurrencyAddress.address);
+        expect(message.payment.cryptocurrency[0].address.type).toBe(testData.PaymentInformation.ItemPrice.CryptocurrencyAddress.type);
+        expect(message.payment.cryptocurrency[0].address.address).toBe(testData.PaymentInformation.ItemPrice.CryptocurrencyAddress.address);
 
         // message.payment.cryptocurrency.shipping_price
         expect(message.payment.cryptocurrency[0].shipping_price).toBeDefined();
@@ -150,9 +136,9 @@ describe('ListingItemFactory', () => {
         expect(message.payment.cryptocurrency[0].shipping_price).not.toHaveProperty('itemPriceId');
         expect(message.payment.cryptocurrency[0].shipping_price).not.toHaveProperty('updatedAt');
         expect(message.payment.cryptocurrency[0].shipping_price).not.toHaveProperty('createdAt');
-        expect(message.payment.cryptocurrency[0].shipping_price.domestic).toBe(listingItemTemplateTestData.PaymentInformation.ItemPrice.ShippingPrice.domestic);
+        expect(message.payment.cryptocurrency[0].shipping_price.domestic).toBe(testData.PaymentInformation.ItemPrice.ShippingPrice.domestic);
         expect(message.payment.cryptocurrency[0].shipping_price.international)
-            .toBe(listingItemTemplateTestData.PaymentInformation.ItemPrice.ShippingPrice.international);
+            .toBe(testData.PaymentInformation.ItemPrice.ShippingPrice.international);
 
         // message.payment.escrow
         expect(message.payment.escrow).toBeDefined();
@@ -161,7 +147,7 @@ describe('ListingItemFactory', () => {
         expect(message.payment.escrow).not.toHaveProperty('updatedAt');
         expect(message.payment.escrow).not.toHaveProperty('createdAt');
         expect(message.payment.escrow).not.toHaveProperty('Ratio');
-        expect(message.payment.escrow.type).toBe(listingItemTemplateTestData.PaymentInformation.Escrow.type);
+        expect(message.payment.escrow.type).toBe(testData.PaymentInformation.Escrow.type);
 
         // message.payment.escrow.ratio
         expect(message.payment.escrow.ratio).toBeDefined();
@@ -169,8 +155,8 @@ describe('ListingItemFactory', () => {
         expect(message.payment.escrow.ratio).not.toHaveProperty('escrowId');
         expect(message.payment.escrow.ratio).not.toHaveProperty('updatedAt');
         expect(message.payment.escrow.ratio).not.toHaveProperty('createdAt');
-        expect(message.payment.escrow.ratio.buyer).toBe(listingItemTemplateTestData.PaymentInformation.Escrow.Ratio.buyer);
-        expect(message.payment.escrow.ratio.seller).toBe(listingItemTemplateTestData.PaymentInformation.Escrow.Ratio.seller);
+        expect(message.payment.escrow.ratio.buyer).toBe(testData.PaymentInformation.Escrow.Ratio.buyer);
+        expect(message.payment.escrow.ratio.seller).toBe(testData.PaymentInformation.Escrow.Ratio.seller);
 
         // message.messaging
         expect(message.messaging).toBeDefined();
@@ -181,27 +167,25 @@ describe('ListingItemFactory', () => {
         expect(message.messaging[0]).not.toHaveProperty('listingItemTemplateId');
         expect(message.messaging[0]).not.toHaveProperty('updatedAt');
         expect(message.messaging[0]).not.toHaveProperty('createdAt');
-        expect(message.messaging[0].protocol).toBe(listingItemTemplateTestData.MessagingInformation[0].protocol);
-        expect(message.messaging[0].public_key).toBe(listingItemTemplateTestData.MessagingInformation[0].publicKey);
+        expect(message.messaging[0].protocol).toBe(testData.MessagingInformation[0].protocol);
+        expect(message.messaging[0].public_key).toBe(testData.MessagingInformation[0].publicKey);
 
         // message.objects
         // TODO: test objects fields
 
+    };
 
+    test('Should get ListingItemMessage', async () => {
+
+        const message: ListingItemMessage = await listingItemFactory
+            .getMessage(listingItemTemplateBasic, listingItemCategoryWithRelatedTestData);
+
+        console.log('message: ', JSON.stringify(message, null, 2));
+
+        // test message conversion
+        expectMessageFromListingItem(message, listingItemTemplateBasic);
     });
-    // tslint:enable:max-line-length
 
-    /*
-        test('Should get the listing-item message from message-processor', () => {
-            const marketId = 1;
-            const result = listingItemFactory.getModel(listingItemForModal, marketId);
-            // check ListingItemCreateRequest
-            expect(result.hash).not.toBeNull();
-            expect(result.market_id).toBe(marketId);
-            expect(result.itemInformation).not.toBe(undefined);
-            expect(result.listingItemObjects).not.toBe(undefined);
-            expect(result.paymentInformation).not.toBe(undefined);
-            expect(result.messagingInformation).not.toBe(undefined);
-        });
-    */
+    // TODO: add tests checking the conversions using different data
+
 });
