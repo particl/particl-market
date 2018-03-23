81798cc2bbc10decb34a22b87767ed1d146486f0
Juha Kovanen <juha.kovanen@gmail.com>
2018-03-23 15:36:13 +0100

    actionmessage createrequest

--- src/api/requests/ActionMessageCreateRequest.ts
+++ src/api/requests/ActionMessageCreateRequest.ts
@@ -1,5 +1,6 @@
 import { IsNotEmpty } from 'class-validator';
 import { RequestBody } from '../../core/api/RequestBody';
+import { MessageObjectCreateRequest } from './MessageObjectCreateRequest';
 
 // tslint:disable:variable-name
 export class ActionMessageCreateRequest extends RequestBody {
@@ -13,5 +14,18 @@ export class ActionMessageCreateRequest extends RequestBody {
     @IsNotEmpty()
     public accepted: boolean;
 
+    @IsNotEmpty()
+    public listing_item_id: number;
+
+    @IsNotEmpty()
+    public message_info_id: number;
+
+    @IsNotEmpty()
+    public message_escrow_id: number;
+
+    @IsNotEmpty()
+    public message_data_id: number;
+
+    public objects: MessageObjectCreateRequest[];
 }
 // tslint:enable:variable-name
