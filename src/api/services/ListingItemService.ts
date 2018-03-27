bfbfbce7fa67c24986aad14f2a1f2cc8e01fc24d
Juha Kovanen <juha.kovanen@gmail.com>
2018-03-27 14:19:12 +0200

    wip: listingitemmessage tests

--- src/api/services/ListingItemService.ts
+++ src/api/services/ListingItemService.ts
@@ -23,29 +23,16 @@ import { PaymentInformationCreateRequest } from '../requests/PaymentInformationC
 import { PaymentInformationUpdateRequest } from '../requests/PaymentInformationUpdateRequest';
 import { MessagingInformationCreateRequest } from '../requests/MessagingInformationCreateRequest';
 import { MessagingInformationUpdateRequest } from '../requests/MessagingInformationUpdateRequest';
-
-import { ListingItemTemplatePostRequest } from '../requests/ListingItemTemplatePostRequest';
-import { ListingItemUpdatePostRequest } from '../requests/ListingItemUpdatePostRequest';
 import { ListingItemObjectCreateRequest } from '../requests/ListingItemObjectCreateRequest';
 import { ListingItemObjectUpdateRequest } from '../requests/ListingItemObjectUpdateRequest';
 
 import { ListingItemTemplateService } from './ListingItemTemplateService';
 import { ListingItemFactory } from '../factories/ListingItemFactory';
 import { SmsgService } from './SmsgService';
-import { Market } from '../models/Market';
 import { FlaggedItem } from '../models/FlaggedItem';
 import { ListingItemObjectService } from './ListingItemObjectService';
 import { FlaggedItemService } from './FlaggedItemService';
-import { NotImplementedException } from '../exceptions/NotImplementedException';
-import { MarketplaceMessageInterface } from '../messages/MarketplaceMessageInterface';
-import { ListingItemMessage } from '../messages/ListingItemMessage';
-import * as resources from 'resources';
 import { EventEmitter } from 'events';
-import { MarketplaceMessage } from '../messages/MarketplaceMessage';
-import { SmsgSendResponse } from '../responses/SmsgSendResponse';
-import { MessageException } from '../exceptions/MessageException';
-import { ItemCategoryCreateRequest } from '../requests/ItemCategoryCreateRequest';
-import {MarketplaceEvent} from '../messages/MarketplaceEvent';
 
 export class ListingItemService {
 
@@ -68,7 +55,6 @@ export class ListingItemService {
         @inject(Types.Core) @named(Core.Logger) public Logger: typeof LoggerType
     ) {
         this.log = new Logger(__filename);
-        this.configureEventListeners();
     }
 
     public async findAll(): Promise<Bookshelf.Collection<ListingItem>> {
@@ -308,126 +294,6 @@ export class ListingItemService {
         }
     }
 
-    /**
-     * post a ListingItem based on a given ListingItem as ListingItemMessage
-     *
-     * @param data
-     * @returns {Promise<void>}
-     */
-    @validate()
-    public async post( @request(ListingItemTemplatePostRequest) data: ListingItemTemplatePostRequest): Promise<SmsgSendResponse> {
-
-        // fetch the listingItemTemplate
-        const itemTemplateModel = await this.listingItemTemplateService.findOne(data.listingItemTemplateId);
-        const itemTemplate = itemTemplateModel.toJSON();
-
-        // this.log.debug('post template: ', JSON.stringify(itemTemplate, null, 2));
-        // get the templates profile address
-        const profileAddress = itemTemplate.Profile.address;
-
-        // fetch the market, will be used later with the broadcast
-        const marketModel: Market = await _.isEmpty(data.marketId)
-            ? await this.marketService.getDefault()
-            : await this.marketService.findOne(data.marketId);
-        const market = marketModel.toJSON();
-
-        // find itemCategory with related
-        const itemCategoryModel = await this.itemCategoryService.findOneByKey(itemTemplate.ItemInformation.ItemCategory.key, true);
-        const itemCategory = itemCategoryModel.toJSON();
-        // this.log.debug('itemCategory: ', JSON.stringify(itemCategory, null, 2));
-
-        const listingItemMessage = await this.listingItemFactory.getMessage(itemTemplate, itemCategory);
-
-        const marketPlaceMessage = {
-            version: process.env.MARKETPLACE_VERSION,
-            item: listingItemMessage
-        } as MarketplaceMessage;
-
-        this.log.debug('post(), marketPlaceMessage: ', marketPlaceMessage);
-        return await this.smsgService.smsgSend(profileAddress, market.address, marketPlaceMessage);
-    }
-
-    /**
-     * update a ListingItem based on a given ListingItem as ListingItemUpdateMessage
-     *
-     * @param data
-     * @returns {Promise<void>}
-     */
-    @validate()
-    public async updatePostItem( @request(ListingItemUpdatePostRequest) data: ListingItemUpdatePostRequest): Promise<void> {
-
-        // TODO: update not implemented/supported yet
-
-        throw new NotImplementedException();
-        /*
-        // fetch the listingItemTemplate
-        const itemTemplateModel = await this.findOne(data.listingItemTemplateId);
-        const itemTemplate = itemTemplateModel.toJSON();
-
-        // get the templates profile address
-        const profileAddress = itemTemplate.Profile.address;
-
-        // check listing-item
-        const listingItems = itemTemplateModel.related('ListingItem').toJSON() || [];
-        if (listingItems.length > 0) {
-            // ListingItemMessage for update
-            const rootCategoryWithRelated: any = await this.itemCategoryService.findRoot();
-            const updateItemMessage = await this.listingItemFactory.getMessage(itemTemplate, rootCategoryWithRelated);
-            updateItemMessage.hash = data.hash; // replace with param hash of listing-item
-
-            // TODO: Need to update broadcast message return after broadcast functionality will be done.
-            this.smsgService.broadcast(profileAddress, market.address, updateItemMessage as ListingItemMessage);
-        } else {
-            this.log.warn(`No listingItem related with listing_item_template_id=${data.hash}!`);
-            throw new MessageException(`No listingItem related with listing_item_template_id=${data.hash}!`);
-        }
-        */
-    }
-
-    /**
-     * processes received ListingItemMessage
-     *
-     * @param {MarketplaceEvent} event
-     * @returns {Promise<"resources".ListingItem>}
-     */
-    public async processListingItemReceivedEvent(event: MarketplaceEvent): Promise<resources.ListingItem> {
-
-        this.log.info('Received event:', event);
-        const message = event.marketplaceMessage;
-
-        if (message.market && message.item) {
-            // get market
-            const marketModel = await this.marketService.findByAddress(message.market);
-            const market = marketModel.toJSON();
-
-            const listingItemMessage = message.item;
-            // create the new custom categories in case there are some
-            const itemCategory: resources.ItemCategory = await this.itemCategoryService.createCategoriesFromArray(listingItemMessage.information.category);
-
-            // find the categories/get the root category with related
-            const rootCategoryWithRelatedModel: any = await this.itemCategoryService.findRoot();
-            const rootCategory = rootCategoryWithRelatedModel.toJSON();
-
-            // create ListingItem
-            const listingItemCreateRequest = await this.listingItemFactory.getModel(listingItemMessage, market.id, rootCategory);
-            // this.log.debug('process(), listingItemCreateRequest:', JSON.stringify(listingItemCreateRequest, null, 2));
-
-            const listingItemModel = await this.create(listingItemCreateRequest);
-            const listingItem = listingItemModel.toJSON();
-
-            // emit the latest message event to cli
-            // this.eventEmitter.emit('cli', {
-            //    message: 'new ListingItem received: ' + JSON.stringify(listingItem)
-            // });
-
-            // this.log.debug('new ListingItem received: ' + JSON.stringify(listingItem));
-            return listingItem;
-
-        } else {
-            throw new MessageException('Marketplace message missing market.');
-        }
-    }
-
     // check if ListingItem already Flagged
     public async isItemFlagged(listingItem: ListingItem): Promise<boolean> {
         const flaggedItem = listingItem.related('FlaggedItem').toJSON();
@@ -447,13 +313,6 @@ export class ListingItemService {
           return itemObject['order'];
         });
         return highestOrder ? highestOrder['order'] : 0;
-    }
-
-    private configureEventListeners(): void {
-        this.eventEmitter.on(Events.ListingItemReceivedEvent, async (event) => {
-            await this.processListingItemReceivedEvent(event);
-        });
-
     }
 
 }
---
+++ test/integration/messageprocessors/ListingItemMessage.test.ts
@@ -0,0 +1,155 @@
+import { app } from '../../../src/app';
+import { Logger as LoggerType } from '../../../src/core/Logger';
+import { Types, Core, Targets } from '../../../src/constants';
+import { TestUtil } from '../lib/TestUtil';
+
+import { NotFoundException } from '../../../src/api/exceptions/NotFoundException';
+
+import { TestDataService } from '../../../src/api/services/TestDataService';
+import { MarketService } from '../../../src/api/services/MarketService';
+import { ListingItemActionService } from '../../../src/api/services/ListingItemActionService';
+
+import { ListingItemFactory } from '../../../src/api/factories/ListingItemFactory';
+
+import { ListingItemMessage } from '../../../src/api/messages/ListingItemMessage';
+
+import * as listingItemSmsg1 from '../../testdata/message/listingItemMessage1.json';
+import * as listingItemSmsg2 from '../../testdata/message/listingItemMessage2.json';
+import * as listingItemSmsg3 from '../../testdata/message/listingItemMessage3.json';
+import * as resources from 'resources';
+
+
+describe('ListingItemMessage', () => {
+    jasmine.DEFAULT_TIMEOUT_INTERVAL = process.env.JASMINE_TIMEOUT;
+
+    const log: LoggerType = new LoggerType(__filename);
+    const testUtil = new TestUtil();
+
+    let testDataService: TestDataService;
+    let marketService: MarketService;
+    let listingItemFactory: ListingItemFactory;
+    let listingItemActionService: ListingItemActionService;
+
+    let defaultMarket;
+
+    // tslint:disable:max-line-length
+    beforeAll(async () => {
+        await testUtil.bootstrapAppContainer(app);  // bootstrap the app
+
+        testDataService = app.IoC.getNamed<TestDataService>(Types.Service, Targets.Service.TestDataService);
+        marketService = app.IoC.getNamed<MarketService>(Types.Service, Targets.Service.MarketService);
+        listingItemFactory = app.IoC.getNamed<ListingItemFactory>(Types.Factory, Targets.Factory.ListingItemFactory);
+
+        listingItemActionService = app.IoC.getNamed<ListingItemActionService>(Types.Service, Targets.Service.ListingItemActionService);
+
+        // clean up the db, first removes all data and then seeds the db with default data
+        await testDataService.clean();
+
+        const defaultMarketModel = await marketService.getDefault();
+        defaultMarket = defaultMarketModel.toJSON();
+
+    });
+    // tslint:enable:max-line-length
+
+    const expectListingItemFromMessage = (result: resources.ListingItem, message: ListingItemMessage) => {
+
+        expect(result.id).not.toBeNull();
+        expect(result.hash).toBe(message.hash);
+
+        // fields from message that we dont want to see
+        expect(result).not.toHaveProperty('information');
+        expect(result).not.toHaveProperty('payment');
+        expect(result).not.toHaveProperty('messaging');
+        expect(result).not.toHaveProperty('objects');
+
+        // ItemInformation
+        expect(result.ItemInformation.title).toBe(message.information.title);
+        expect(result.ItemInformation.shortDescription).toBe(message.information.short_description);
+        expect(result.ItemInformation.longDescription).toBe(message.information.long_description);
+
+        // ItemInformation.ItemCategory
+        expect(result.ItemInformation.ItemCategory.key).toBe(message.information.category[2]);
+        expect(result.ItemInformation.ItemCategory.parentItemCategoryId).not.toBeNull();
+
+        // ItemInformation.ItemLocation
+        expect(result.ItemInformation.ItemLocation.region).toBe(message.information.location.country);
+        expect(result.ItemInformation.ItemLocation.address).toBe(message.information.location.address);
+
+        // ItemInformation.ItemLocation.LocationMarker
+        expect(result.ItemInformation.ItemLocation.LocationMarker.markerTitle).toBe(message.information.location.gps.marker_title);
+        expect(result.ItemInformation.ItemLocation.LocationMarker.markerText).toBe(message.information.location.gps.marker_text);
+        expect(result.ItemInformation.ItemLocation.LocationMarker.lat).toBe(message.information.location.gps.lat);
+        expect(result.ItemInformation.ItemLocation.LocationMarker.lng).toBe(message.information.location.gps.lng);
+
+        // ItemInformation.ShippingDestinations
+        expect(result.ItemInformation.ShippingDestinations.length).toBe(message.information.shipping_destinations.length);
+        // todo: test the shipping destinations
+        // expect(message.information.shipping_destinations).toContain('-MOROCCO');
+        // expect(message.information.shipping_destinations).toContain('PANAMA');
+
+        // ItemInformation.ItemImages
+        expect(result.ItemInformation.ItemImages.length).toBe(5);
+        // todo: test the images
+        // expect(message.information.images[0].hash).toBe(testData.ItemInformation.ItemImages[0].hash);
+        // expect(message.information.images[0].data.length).toBe(1);
+        // expect(message.information.images[0].data[0].protocol).toBe(testData.ItemInformation.ItemImages[0].ItemImageDatas[0].protocol);
+        // expect(message.information.images[0].data[0].encoding).toBe(testData.ItemInformation.ItemImages[0].ItemImageDatas[0].encoding);
+        // expect(message.information.images[0].data[0].data).toBe(testData.ItemInformation.ItemImages[0].ItemImageDatas[0].data);
+
+        // PaymentInformation
+        expect(result.PaymentInformation.type).toBe(message.payment.type);
+
+        // PaymentInformation.Escrow
+        expect(result.PaymentInformation.Escrow.type).toBe(message.payment.escrow.type);
+
+        // PaymentInformation.Escrow.Ratio
+        expect(result.PaymentInformation.Escrow.Ratio.buyer).toBe(message.payment.escrow.ratio.buyer);
+        expect(result.PaymentInformation.Escrow.Ratio.seller).toBe(message.payment.escrow.ratio.seller);
+
+        // PaymentInformation.ItemPrice
+        const itemPrice = result.PaymentInformation.ItemPrice;
+        expect(itemPrice.currency).toBe(message.payment.cryptocurrency[0].currency);
+        expect(itemPrice.basePrice).toBe(message.payment.cryptocurrency[0].base_price);
+
+        // PaymentInformation.ItemPrice.CryptocurrencyAddress
+        const cryptocurrencyAddress = result.PaymentInformation.ItemPrice.CryptocurrencyAddress;
+        expect(cryptocurrencyAddress.type).toBe(message.payment.cryptocurrency[0].address.type);
+        expect(cryptocurrencyAddress.address).toBe(message.payment.cryptocurrency[0].address.address);
+
+        // PaymentInformation.ItemPrice.ShippingPrice
+        const shippingPrice = result.PaymentInformation.ItemPrice.ShippingPrice;
+        expect(shippingPrice.domestic).toBe(message.payment.cryptocurrency[0].shipping_price.domestic);
+        expect(shippingPrice.international).toBe(message.payment.cryptocurrency[0].shipping_price.international);
+
+        // MessagingInformation
+        expect(result.MessagingInformation[0].protocol).toBe(message.messaging[0].protocol);
+        expect(result.MessagingInformation[0].publicKey).toBe(message.messaging[0].public_key);
+
+        // listingitem-object
+        // TODO test listingitemobjects
+        // expect(result.ListingItemObjects[0].type).toBe(message.objects[0].type);
+        // expect(result.ListingItemObjects[0].description).toBe(message.objects[0].description);
+        // expect(result.ListingItemObjects[0].order).toBe(message.objects[0].order);
+    };
+
+    test('Should process MarketplaceEvent containing ListingItemMessage', async () => {
+
+        const marketplaceMessage = JSON.parse(listingItemSmsg1.text);
+        marketplaceMessage.market = listingItemSmsg1.to;
+
+        const result = await listingItemActionService.processListingItemReceivedEvent({
+            smsgMessage: listingItemSmsg1,
+            marketplaceMessage
+        });
+
+        // log.debug('result: ', JSON.stringify(result, null, 2));
+        log.debug('listingItemMessage: ', JSON.stringify(marketplaceMessage.item, null, 2));
+        log.debug('result.hash: ', JSON.stringify(result.hash, null, 2));
+        log.debug('listingItemMessage.hash: ', JSON.stringify(marketplaceMessage.item.hash, null, 2));
+        expectListingItemFromMessage(result, marketplaceMessage.item);
+
+    });
+
+    // todo: test with different types of data
+
+});
