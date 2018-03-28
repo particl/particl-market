f908734941e5e29fbaed407b6544e3f77477d25b
navjot.g <navjot.g@cisinlabs.com>
2018-03-28 14:11:30 +0530

    MessageData integration test DONE

--- test/integration/MessageData.test.ts
+++ test/integration/MessageData.test.ts
@@ -8,8 +8,20 @@ import { ValidationException } from '../../src/api/exceptions/ValidationExceptio
 import { NotFoundException } from '../../src/api/exceptions/NotFoundException';
 
 import { MessageData } from '../../src/api/models/MessageData';
+import { ActionMessage } from '../../src/api/models/ActionMessage';
 
 import { MessageDataService } from '../../src/api/services/MessageDataService';
+import { ActionMessageCreateRequest } from '../../src/api/requests/ActionMessageCreateRequest';
+import { MessageDataCreateRequest } from '../../src/api/requests/MessageDataCreateRequest';
+import { MessageDataUpdateRequest } from '../../src/api/requests/MessageDataUpdateRequest';
+import { GenerateListingItemParams } from '../../src/api/requests/params/GenerateListingItemParams';
+import { CreatableModel } from '../../src/api/enums/CreatableModel';
+import { TestDataGenerateRequest } from '../../src/api/requests/TestDataGenerateRequest';
+import { MessageInfoCreateRequest } from '../../src/api/requests/MessageInfoCreateRequest';
+import { MessageEscrowCreateRequest } from '../../src/api/requests/MessageEscrowCreateRequest';
+import { MessageObjectCreateRequest } from '../../src/api/requests/MessageObjectCreateRequest';
+import { ActionMessageService } from '../../src/api/services/ActionMessageService';
+
 
 describe('MessageData', () => {
     jasmine.DEFAULT_TIMEOUT_INTERVAL = process.env.JASMINE_TIMEOUT;
@@ -19,8 +31,10 @@ describe('MessageData', () => {
 
     let testDataService: TestDataService;
     let messageDataService: MessageDataService;
+    let actionMessageService: ActionMessageService;
 
-    // let createdId;
+    let createdMessageData;
+    let createdActionMessageId;
 
     const testData = {
         msgid: 'fdd0b25a000000007188f0fc4cd57a37aa5a9ab26463510568e99d7d',
@@ -29,7 +43,24 @@ describe('MessageData', () => {
         sent: new Date(),
         from: 'piyLdJcTzR72DsYh2j5wPWUUmwURfczTR3',
         to: 'pmktyVZshdMAQ6DPbbRXEFNGuzMbTMkqAA'
-    };
+    } as MessageDataCreateRequest;
+
+    const testDataActionMessage = {
+        action: 'MPA_LOCK',
+        nonce: 'randomness',
+        accepted: true,
+        info: {
+            address: '20 seventeen street, march city, 2017',
+            memo: 'Please deliver by 17 March 2017'
+        } as MessageInfoCreateRequest,
+        escrow: {
+            rawtx: 'rawtx',
+            type: 'refund'
+        } as MessageEscrowCreateRequest,
+        data: testData,
+        objects: []
+    } as ActionMessageCreateRequest;
+
 
     const testDataUpdated = {
         msgid: 'fdd0b25a000000007188f0fc4cd57a37aa5a9ab26463510568e99d7d_UPDATED',
@@ -38,49 +69,68 @@ describe('MessageData', () => {
         sent: new Date(),
         from: 'piyLdJcTzR72DsYh2j5wPWUUmwURfczTR3_UPDATED',
         to: 'pmktyVZshdMAQ6DPbbRXEFNGuzMbTMkqAA_UPDATED'
-    };
+    } as MessageDataUpdateRequest;
 
     beforeAll(async () => {
         await testUtil.bootstrapAppContainer(app);  // bootstrap the app
 
         testDataService = app.IoC.getNamed<TestDataService>(Types.Service, Targets.Service.TestDataService);
         messageDataService = app.IoC.getNamed<MessageDataService>(Types.Service, Targets.Service.MessageDataService);
+        actionMessageService = app.IoC.getNamed<ActionMessageService>(Types.Service, Targets.Service.ActionMessageService);
 
         // clean up the db, first removes all data and then seeds the db with default data
         await testDataService.clean();
+
+        const generateParams = new GenerateListingItemParams([
+            true,   // generateItemInformation
+            true,   // generateShippingDestinations
+            true,   // generateItemImages
+            true,   // generatePaymentInformation
+            true,   // generateEscrow
+            true,   // generateItemPrice
+            true,   // generateMessagingInformation
+            true    // generateListingItemObjects
+        ]).toParamsArray();
+
+        // create listingitem without ShippingDestinations and store its id for testing
+        const listingItems = await testDataService.generate({
+            model: CreatableModel.LISTINGITEM,  // what to generate
+            amount: 1,                          // how many to generate
+            withRelated: true,                  // return model
+            generateParams                      // what kind of data to generate
+        } as TestDataGenerateRequest);
+        const createdListingItem = listingItems[0].toJSON();
+
+        // create actionMessage
+        testDataActionMessage.listing_item_id = createdListingItem.id;
     });
 
     afterAll(async () => {
         //
     });
 
-    test('Should throw ValidationException because there is no related_id', async () => {
-        expect.assertions(1);
-        await messageDataService.create(testData).catch(e =>
-            expect(e).toEqual(new ValidationException('Request body is not valid', []))
-        );
-    });
-/*
+
     test('Should create a new message data', async () => {
-        // testData['related_id'] = 0;
-        const messageDataModel: MessageData = await messageDataService.create(testData);
-        createdId = messageDataModel.Id;
+        let actionMessageModel: ActionMessage = await actionMessageService.create(testDataActionMessage);
 
-        const result = messageDataModel.toJSON();
+        actionMessageModel = actionMessageModel.toJSON();
+        createdActionMessageId = actionMessageModel.id;
+        const result: any = actionMessageModel.MessageData;
+        createdMessageData = result;
 
-        // test the values
-        // expect(result.value).toBe(testData.value);
-        expect(result.msgid).toBe(testData.msgid);
-        expect(result.version).toBe(testData.version);
-        expect(result.received).toBe(testData.received);
-        expect(result.sent).toBe(testData.sent);
+        // MessageData
+        expect(result.actionMessageId).toBe(createdActionMessageId);
         expect(result.from).toBe(testData.from);
+        expect(result.msgid).toBe(testData.msgid);
+        expect(result.received).toBeDefined();
+        expect(result.sent).toBeDefined();
         expect(result.to).toBe(testData.to);
+        expect(result.version).toBe(testData.version);
     });
 
     test('Should throw ValidationException because we want to create a empty message data', async () => {
         expect.assertions(1);
-        await messageDataService.create({}).catch(e =>
+        await messageDataService.create({} as MessageDataCreateRequest).catch(e =>
             expect(e).toEqual(new ValidationException('Request body is not valid', []))
         );
     });
@@ -93,50 +143,54 @@ describe('MessageData', () => {
         const result = messageData[0];
 
         // test the values
-        // expect(result.value).toBe(testData.value);
-        expect(result.msgid).toBe(testData.msgid);
-        expect(result.version).toBe(testData.version);
-        expect(result.received).toBe(testData.received);
-        expect(result.sent).toBe(testData.sent);
+        expect(result.actionMessageId).toBe(createdActionMessageId);
         expect(result.from).toBe(testData.from);
+        expect(result.msgid).toBe(testData.msgid);
+        expect(result.received).toBeDefined();
+        expect(result.sent).toBeDefined();
         expect(result.to).toBe(testData.to);
+        expect(result.version).toBe(testData.version);
     });
 
     test('Should return one message data', async () => {
-        const messageDataModel: MessageData = await messageDataService.findOne(createdId);
+        const messageDataModel: MessageData = await messageDataService.findOne(createdMessageData.id);
         const result = messageDataModel.toJSON();
 
         // test the values
-        // expect(result.value).toBe(testData.value);
-        expect(result.msgid).toBe(testData.msgid);
-        expect(result.version).toBe(testData.version);
-        expect(result.received).toBe(testData.received);
-        expect(result.sent).toBe(testData.sent);
+        expect(result.actionMessageId).toBe(createdActionMessageId);
         expect(result.from).toBe(testData.from);
+        expect(result.msgid).toBe(testData.msgid);
+        expect(result.received).toBeDefined();
+        expect(result.sent).toBeDefined();
         expect(result.to).toBe(testData.to);
+        expect(result.version).toBe(testData.version);
     });
 
     test('Should update the message data', async () => {
-        // testDataUpdated['related_id'] = 0;
-        const messageDataModel: MessageData = await messageDataService.update(createdId, testDataUpdated);
+        const messageDataModel: MessageData = await messageDataService.update(createdMessageData.id, testDataUpdated);
         const result = messageDataModel.toJSON();
 
         // test the values
-        // expect(result.value).toBe(testDataUpdated.value);
+        expect(result.actionMessageId).toBe(createdActionMessageId);
         expect(result.msgid).toBe(testDataUpdated.msgid);
         expect(result.version).toBe(testDataUpdated.version);
-        expect(result.received).toBe(testDataUpdated.received);
-        expect(result.sent).toBe(testDataUpdated.sent);
+        expect(result.received).not.toBe(createdMessageData.received);
+        expect(result.sent).not.toBe(createdMessageData.sent);
         expect(result.from).toBe(testDataUpdated.from);
         expect(result.to).toBe(testDataUpdated.to);
     });
 
     test('Should delete the message data', async () => {
-        expect.assertions(1);
-        await messageDataService.destroy(createdId);
-        await messageDataService.findOne(createdId).catch(e =>
-            expect(e).toEqual(new NotFoundException(createdId))
+        expect.assertions(2);
+        await messageDataService.destroy(createdMessageData.id);
+        await messageDataService.findOne(createdMessageData.id).catch(e =>
+            expect(e).toEqual(new NotFoundException(createdMessageData.id))
         );
+
+        await actionMessageService.destroy(createdActionMessageId);
+        await actionMessageService.findOne(createdActionMessageId).catch(e =>
+            expect(e).toEqual(new NotFoundException(createdActionMessageId))
+        );
+
     });
-*/
 });
