c82546741da95f24ca1de33e35dac56f31c4886e
Juha Kovanen <juha.kovanen@gmail.com>
2018-03-31 19:40:16 +0200

    Should post a ListingItem in to the default marketplace without LocationMarker

--- test/black-box/listingItemtemplate/ListingItemTemplatePostCommand.test.ts
+++ test/black-box/listingItemtemplate/ListingItemTemplatePostCommand.test.ts
@@ -5,14 +5,7 @@ import { Commands } from '../../../src/api/commands/CommandEnumType';
 import { CreatableModel } from '../../../src/api/enums/CreatableModel';
 import { GenerateListingItemTemplateParams } from '../../../src/api/requests/params/GenerateListingItemTemplateParams';
 
-import * as listingItemCreateRequestBasic1 from '../../testdata/createrequest/listingItemCreateRequestBasic1.json';
-import * as listingItemCreateRequestBasic2 from '../../testdata/createrequest/listingItemCreateRequestBasic2.json';
-import * as listingItemCreateRequestBasic3 from '../../testdata/createrequest/listingItemCreateRequestBasic3.json';
-
-import * as listingItemUpdateRequestBasic1 from '../../testdata/pdaterequest/listingItemUpdateRequestBasic1.json';
-
-import * as listingItemTemplateCreateRequestBasic1 from '../../testdata/createrequest/listingItemTemplateCreateRequestBasic1.json';
-import * as listingItemTemplateCreateRequestBasic2 from '../../testdata/createrequest/listingItemTemplateCreateRequestBasic2.json';
+import * as listingItemTemplateCreateRequestWithoutLocationMarker from '../../testdata/createrequest/listingItemTemplateCreateRequestWithoutLocationMarker.json';
 
 import * as resources from 'resources';
 
@@ -61,6 +54,25 @@ describe('ListingItemSearchCommand', () => {
     });
 
     test('Should post a ListingItem in to the default marketplace', async () => {
+        postedTemplateId = listingItemTemplates[0].id;
+        const res: any = await rpc(templateCommand, [templatePostCommand, postedTemplateId, defaultMarket.id]);
+        res.expectJson();
+        res.expectStatusCode(200);
+
+        const result: any = res.getBody()['result'];
+        expect(result.result).toBe('Sent.');
+        expect(result.txid).toBeDefined();
+        expect(result.fee).toBeGreaterThan(0);
+    });
+
+    test('Should post a ListingItem in to the default marketplace without LocationMarker', async () => {
+
+        // generate listingItemTemplate
+        listingItemTemplates = await testUtil.addData(
+            CreatableModel.LISTINGITEMTEMPLATE,
+            listingItemTemplateCreateRequestWithoutLocationMarker
+        ) as resources.ListingItemTemplates[];
+
         postedTemplateId = listingItemTemplates[0].id;
         const res: any = await rpc(templateCommand, [templatePostCommand, postedTemplateId, defaultMarket.id]);
         res.expectJson();
