0b9fd14d96e8fba52131525a4c4e30b98f1dd439
Cube <cryptocube@particl.io>
2018-04-23 20:10:35 +0930

    Fixed ProfileUpdateCommand blackbox test

--- test/black-box/profile/ProfileUpdateCommand.test.ts
+++ test/black-box/profile/ProfileUpdateCommand.test.ts
@@ -2,6 +2,7 @@ import { rpc, api } from '../lib/api';
 import { BlackBoxTestUtil } from '../lib/BlackBoxTestUtil';
 import { Commands } from '../../../src/api/commands/CommandEnumType';
 import { CreatableModel } from '../../../src/api/enums/CreatableModel';
+import { MessageException } from '../../../src/api/exceptions/MessageException';
 
 describe('ProfileUpdateCommand', () => {
 
@@ -41,19 +42,21 @@ describe('ProfileUpdateCommand', () => {
 
     test('Should update the profile by RPC', async () => {
         // set up the test data
-        const addDataRes: any = await testUtil.addData(CreatableModel.PROFILE, testData);
-        const createdId = addDataRes.id;
+        let generatedProfile: any = await testUtil.generateData(CreatableModel.PROFILE, 1, true);
+        generatedProfile = generatedProfile[0];
+        const createdId = generatedProfile.id;
 
         // update profile
         const profileName = 'UPDATED-DEFAULT-PROFILE-TEST';
         const profileAddress = 'UPDATED-DEFAULT-PROFILE-TEST-ADDRESS';
-        const res = await rpc(method, [subCommand, createdId, profileName, profileAddress]);
+        const res = await rpc(method, [subCommand, createdId, profileName]);
+        console.log(JSON.stringify(res));
         res.expectJson();
         res.expectStatusCode(200);
         const result: any = res.getBody()['result'];
 
         expect(result.name).toBe(profileName);
-        expect(result.address).toBe('DEFAULT-PROFILE-ADDRESS'); // we are not allowing the address to be updated
+        expect(result.address).toBe(generatedProfile.address); // we are not allowing the address to be updated
     });
 
 });
