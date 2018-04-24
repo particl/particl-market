7cc56f80a010b01bf166d9b128b201c96d8d5ade
Kewde <kewde@particl.io>
2018-04-24 21:07:41 +0200

    module

---
+++ src/module.ts
@@ -0,0 +1,14 @@
+import 'reflect-metadata';
+import { App } from './core/App';
+import { CustomConfig } from './config/CustomConfig';
+
+
+exports.start = () => {
+    const app = new App();
+    // Here you can add more custom configurations
+    app.configure(new CustomConfig());
+    // Launch the server with all his awesome features.
+    
+    app.bootstrap();
+    return app;
+};
