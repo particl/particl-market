0d9c98f72c3a4f78de8d22b44b4c083d66ab841b
Cube <cryptocube@particl.io>
2018-08-03 20:13:46 +1000

    Clean up test and get block number

--- test/black-box/proposal/ProposalPost.test.ts
+++ test/black-box/proposal/ProposalPost.test.ts
@@ -9,7 +9,7 @@ import { Profile } from '../../../src/api/models/Profile';
 import * as Faker from 'faker';
 
 describe('ProposalPost', () => {
-    jasmine.DEFAULT_TIMEOUT_INTERVAL = 10 * process.env.JASMINE_TIMEOUT;
+    jasmine.DEFAULT_TIMEOUT_INTERVAL = 100 * process.env.JASMINE_TIMEOUT;
 
     const log: LoggerType = new LoggerType(__filename);
 
@@ -17,9 +17,10 @@ describe('ProposalPost', () => {
     const proposalMethod = Commands.PROPOSAL_ROOT.commandName;
     const proposalPostSubCommand = Commands.PROPOSAL_POST.commandName;
     const proposalListSubCommand = Commands.PROPOSAL_LIST.commandName;
+    const daemonMethod = Commands.DAEMON_ROOT.commandName;
 
     let defaultProfile: Profile;
-    // let createdListingItemTemplate: ListingItemTemplate;
+    let currentBlockNumber;
 
     beforeAll(async () => {
         await testUtil.cleanDb();
@@ -27,49 +28,42 @@ describe('ProposalPost', () => {
         // TODO: defaultProfile might not be the correct one
         defaultProfile = await testUtil.getDefaultProfile();
 
-        /*const generateListingItemTemplateParams = new GenerateListingItemTemplateParams([
-            true,   // generateItemInformation
-            false,  // generateShippingDestinations
-            false,   // generateItemImages
-            true,   // generatePaymentInformation
-            true,   // generateEscrow
-            true,   // generateItemPrice
-            true,   // generateMessagingInformation
-            true    // generateListingItemObjects
-        ]).toParamsArray();
-
-        // create template without shipping destinations and store its id for testing
-        const listingItemTemplates = await testUtil.generateData(
-            CreatableModel.LISTINGITEMTEMPLATE,     // what to generate
-            1,                              // how many to generate
-            true,                        // return model
-            generateListingItemTemplateParams       // what kind of data to generate
-        ) as ListingItemTemplate[];
-        createdListingItemTemplate = listingItemTemplates[0];*/
-
-        // TODO: Get current block and save it for the post test
+        {
+            const response: any = await testUtil.rpc(daemonMethod, [
+                'getblockcount'
+            ]);
+            response.expectJson();
+            response.expectStatusCode(200);
+            const result: any = response.getBody()['result'];
+            currentBlockNumber = result;
+        }
     });
 
     test('Should post a proposal', async () => {
-        const testData = testUtil.generateData(CreatableModel.PROPOSAL, 1, false, []);
-        const title = Faker.lorem.words(); // TODO: Might want to generate this
+        const title = Faker.lorem.words();
         const description = Faker.lorem.paragraph();
-        let response: any = await testUtil.rpc(proposalMethod, [
-            proposalPostSubCommand,
-            defaultProfile.id,
-            title,
-            description,
-            0,
-            1000000, // TODO: This needs to be made dynamic
-            'optionA',
-            'optionB'
-        ]);
-        response.expectJson();
-        response.expectStatusCode(200);
-        const result: any = response.getBody()['result'];
-        expect(result.result).toEqual('Sent.');
+        const startBlockNumber = currentBlockNumber - 1;
+        const endtBlockNumber = currentBlockNumber + 100;
+        const optionA = 'optionA';
+        const optionB = 'optionB';
+        {
+            const response: any = await testUtil.rpc(proposalMethod, [
+                proposalPostSubCommand,
+                defaultProfile.id,
+                title,
+                description,
+                startBlockNumber,
+                endtBlockNumber, // TODO: This needs to be made dynamic
+                optionA,
+                optionB
+            ]);
+            response.expectJson();
+            response.expectStatusCode(200);
+            const result: any = response.getBody()['result'];
+            expect(result.result).toEqual('Sent.');
+        }
 
-        response = await testUtil.rpcWaitFor(proposalMethod,
+        const response = await testUtil.rpcWaitFor(proposalMethod,
             [
                 proposalListSubCommand,
             ],
