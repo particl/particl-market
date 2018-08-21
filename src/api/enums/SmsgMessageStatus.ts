da7336050cb774b7fd1e3770e2b2218d578b39a0
Juha Kovanen <juha.kovanen@gmail.com>
2018-08-21 10:38:46 +0300

    smsgmessagestatus

---
+++ src/api/enums/SmsgMessageStatus.ts
@@ -0,0 +1,17 @@
+// Copyright (c) 2017-2018, The Particl Market developers
+// Distributed under the GPL software license, see the accompanying
+// file COPYING or https://github.com/particl/particl-market/blob/develop/LICENSE
+
+/**
+ * SmsgMessageStatus
+ *
+ */
+
+export enum SmsgMessageStatus {
+
+    NEW = 'NEW',
+    PROCESSED = 'PROCESSED',
+    PROCESSING_FAILED = 'PROCESSING_FAILED',
+    WAITING = 'WAITING'
+
+}
