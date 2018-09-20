df95013cec0c4c4dd0e7b3dc640b59855f169a2b
Juha Kovanen <juha.kovanen@gmail.com>
2018-09-20 04:33:30 +0300

    buyflow

--- test/black-box/BuyFlow.test.ts
+++ test/black-box/BuyFlow.test.ts
@@ -226,6 +226,10 @@ describe('Happy Buy Flow', () => {
         log.debug('listingItemTemplatesSellerNode[0].hash: ', listingItemTemplatesSellerNode[0].hash);
         expect(result.hash).toBe(listingItemTemplatesSellerNode[0].hash);
         log.debug('result.ListingItemTemplate.hash: ', listingItemTemplatesSellerNode[0].hash);
+
+        if (result.ListingItemTemplate === null) {
+            log.debug('WHYYYY: ', JSON.stringify(result, null,2 ));
+        }
         expect(result.ListingItemTemplate.hash).toBe(listingItemTemplatesSellerNode[0].hash);
         // sometimes result.ListingItemTemplate is null!!!
 
@@ -637,8 +641,6 @@ describe('Happy Buy Flow', () => {
         log.debug('==> Updated Bid found on BUYER node.');
     });
 
-// omp-lib needs to be integrated for rest of the tests to work
-/*
     test('BUYER POSTS MPA_LOCK', async () => {
 
         log.debug('========================================================================================');
@@ -994,6 +996,5 @@ describe('Happy Buy Flow', () => {
         log.debug('==> No locked outputs left.');
 
     }, 600000); // timeout to 600s
-*/
 
 });
