661cf170dda43f9091645f29dbc8570ea6d7923b
lazaruscrypto <vlad@particl.io>
2018-10-10 16:18:43 +0300

    Local bug

--- dist 2/api/messages/ActionMessageInterface.js
+++
@@ -1,6 +0,0 @@
-"use strict";
-// Copyright (c) 2017-2018, The Particl Market developers
-// Distributed under the GPL software license, see the accompanying
-// file COPYING or https://github.com/particl/particl-market/blob/develop/LICENSE
-Object.defineProperty(exports, "__esModule", { value: true });
-//# sourceMappingURL=ActionMessageInterface.js.map
