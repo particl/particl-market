a94a8b09003f5fd69484a91457b4f8ea51e9034c
Juha Kovanen <juha.kovanen@gmail.com>
2018-12-04 19:06:20 -0500

    integration_test_develop dockerize

--- bin/ci-integration-tests.sh
+++ bin/ci-integration-tests.sh
@@ -21,24 +21,7 @@ cat .env.test
 
 yarn install --check-files
 
-export NODE_ENV=$1
-export APP_PORT=$2
-export APP_HOST=$3
-export RPCUSER=$4
-export RPCPASSWORD=$5
-export RPCHOSTNAME=$6
-export MAINNET_PORT=$7
-export TESTNET_PORT=$8
-
-echo $NODE_ENV
-echo $APP_PORT
-echo $APP_HOST
-echo $RPCUSER
-echo $RPCPASSWORD
-echo $RPCHOSTNAME
-echo $MAINNET_PORT
-echo $TESTNET_PORT
-
 # ./dockerize -wait $APP_HOST:$APP_PORT/cli -timeout 30s
+# ./dockerize -wait tcp://$RPCHOSTNAME:$TESTNET_PORT -timeout 30s
 
 npm run test:integration:pretty
