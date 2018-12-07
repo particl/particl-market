de81b461e5522893ccd23d664e26aa01e84ec597
Juha Kovanen <juha.kovanen@gmail.com>
2018-12-07 06:11:02 -0500

    circleci config cleanup, volumes for containers, env configs, ...

--- bin/ci-blackbox-tests.sh
+++ bin/ci-blackbox-tests.sh
@@ -6,6 +6,8 @@ cp -f .env.circle.app1 .env
 cp -f .env.circle.test .env.test
 cp -f .env.circle.blackbox.example .env.blackbox
 
+yarn install --check-files
+
 # wait until core rpc and the mp cli are up
 ./dockerize -wait tcp://circle.particl.xyz:58935 -timeout 60s
 echo "connected to: tcp://circle.particl.xyz:58935"
@@ -16,5 +18,4 @@ echo "connected to: http://circle.particl.xyz:3100"
 ./dockerize -wait http://circle.particl.xyz:3200/cli/ -timeout 600s
 echo "connected to: http://circle.particl.xyz:3200"
 
-yarn install --check-files
 npm run test:black-box:pretty
