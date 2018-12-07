2f795615d6d7e58033ec60c5cead48562eec20dd
Juha Kovanen <juha.kovanen@gmail.com>
2018-12-06 19:52:04 -0500

    kontena config

--- bin/ci-blackbox-tests.sh
+++ bin/ci-blackbox-tests.sh
@@ -7,11 +7,11 @@ cp -f .env.circle.test .env.test
 cp -f .env.circle.blackbox.example .env.blackbox
 
 yarn install --check-files
-./dockerize -wait tcp://circle.particl.xyz:58935 -timeout 30s
-./dockerize -wait tcp://circle.particl.xyz:59935 -timeout 30s
 
-#todo ports
-./dockerize -wait http://circle.particl.xyz:3000 -timeout 30s
-./dockerize -wait http://circle.particl.xyz:3000 -timeout 30s
+# wait until core rpc and the mp cli are up
+./dockerize -wait tcp://circle.particl.xyz:58935 -timeout 60s
+./dockerize -wait tcp://circle.particl.xyz:59935 -timeout 60s
+./dockerize -wait http://circle.particl.xyz:3100/cli -timeout 60s
+./dockerize -wait http://circle.particl.xyz:3200/cli -timeout 60s
 
 npm run test:black-box:pretty
