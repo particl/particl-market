69549add9c753601d66271f303a2a4bb6b4dbcbd
Juha Kovanen <juha.kovanen@gmail.com>
2018-12-07 07:18:35 -0500

    uitest

--- bin/ci-ui-tests.sh
+++ bin/ci-ui-tests.sh
@@ -4,7 +4,7 @@ set -e
 # setup config files
 cp -f .env.circle.app1 .env
 cp -f .env.circle.test .env.test
-cp -f .env.blackbox.example .env.blackbox
+cp -f .env.circle.blackbox.example .env.blackbox
 
 yarn install --check-files
 
