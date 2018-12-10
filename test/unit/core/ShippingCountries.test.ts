2e0f6822d267f602d64aeee8d6c78ca5cb4e8474
Cube <cryptocube@particl.io>
2018-12-11 01:06:41 +1100

    Added more validation for country names/codes

--- test/unit/core/ShippingCountries.test.ts
+++ test/unit/core/ShippingCountries.test.ts
@@ -7,6 +7,7 @@ import { ShippingCountries } from '../../../src/core/helpers/ShippingCountries';
 import { getDataSet, reduce } from 'iso3166-2-db';
 
 this.countryCodeList = reduce(getDataSet(), 'en');
+this.countryList = ShippingCountries.countryList;
 this.invalidCountryCodes = ['ASDF', 'A', 'ASD', '1', '11', 'Z1', 'Z11'];
 // Countries that never or no longer exist, lost their independence, or have changed their name.
 this.invalidCountries = ['Cobrastan', 'Ottoman Empire', 'Rhodesia', 'Czechoslovakia', 'Tibet', 'Yugoslavia', 'Burma'];
@@ -15,9 +16,25 @@ this.questionableCountries = ['Europe', 'Asia'];
 
 describe('ShippingCountries', () => {
     test('isValidCountryCode() should return true for all country codes', () => {
+        // Check valid country codes
         for ( const x in this.countryCodeList ) {
             if ( x ) {
+                // Check country code is correct
                 expect(ShippingCountries.isValidCountryCode(x)).toBe(true);
+                // Check country from country code is correct
+                expect(ShippingCountries.isValidCountry(ShippingCountries.getCountry(x))).toBe(true);
+            }
+        }
+    });
+
+    test('isValidCountry() should return true for all country names', () => {
+        // Check valid contry names
+        for ( const x in this.countryList ) {
+            if (x) {
+                // Check country name is valid
+                expect(ShippingCountries.isValidCountry(x)).toBe(true);
+                // Check country code from country name is valid
+                expect(ShippingCountries.isValidCountryCode(ShippingCountries.getCountryCode(x))).toBe(true);
             }
         }
     });
