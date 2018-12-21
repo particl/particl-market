ff3015a5afd5b6f8f9f8ca5674e84a4a5648addc
Juha Kovanen <juha.kovanen@gmail.com>
2018-12-21 09:27:20 -0500

    droneci

--- bin/ci-unit-tests.sh
+++ bin/ci-unit-tests.sh
@@ -6,5 +6,8 @@ cp -f .env.circle.app1 .env
 cp -f .env.circle.test .env.test
 cp -f .env.circle.blackbox .env.blackbox
 
+echo '---------------------------------------------------------------'
+npm -v
+echo '---------------------------------------------------------------'
 yarn install --check-files
 npm test
