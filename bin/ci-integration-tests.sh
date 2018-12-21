482680b37200715e829254cef57c808693570859
Juha Kovanen <juha.kovanen@gmail.com>
2018-12-21 14:05:07 -0500

    droneci

--- bin/ci-integration-tests.sh
+++ bin/ci-integration-tests.sh
@@ -6,6 +6,7 @@ cp -f .env.circle.app1 .env
 cp -f .env.circle.test .env.test
 cp -f .env.circle.blackbox .env.blackbox
 
+"mysql": "^2.13.0"
 yarn install --check-files
 ./dockerize -wait tcp://circle.particl.xyz:58935 -timeout 30s
 npm run test:integration:pretty
