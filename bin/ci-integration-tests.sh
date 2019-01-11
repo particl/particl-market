67cf12ff877338ef402e3e87d6ed142b273679c6
Juha Kovanen <juha.kovanen@gmail.com>
2019-01-11 13:03:56 -0500

    removed unnecessary dockerize wait for port

--- bin/ci-integration-tests.sh
+++ bin/ci-integration-tests.sh
@@ -10,6 +10,5 @@ yarn install --check-files
 ./node_modules/.bin/jest -v
 
 ./dockerize -wait tcp://circle.particl.xyz:58935 -timeout 30s
-./dockerize -wait tcp://circle.particl.xyz:33306 -timeout 30s
 
 npm run test:integration:pretty
