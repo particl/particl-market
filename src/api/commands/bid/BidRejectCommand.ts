1f5fc657f0cb8cfeafbc31d4721499eeb6d563ba
Cube <cube@cubik.tech>
2019-01-29 18:56:52 +1100

    Reupload PR

--- src/api/commands/bid/BidRejectCommand.ts
+++ src/api/commands/bid/BidRejectCommand.ts
@@ -17,6 +17,7 @@ import { BaseCommand } from '../BaseCommand';
 import { BidActionService } from '../../services/BidActionService';
 import { SmsgSendResponse } from '../../responses/SmsgSendResponse';
 import { BidService } from '../../services/BidService';
+import { BidRejectReason } from '../../enums/BidRejectReason';
 
 export class BidRejectCommand extends BaseCommand implements RpcCommandInterface<SmsgSendResponse> {
 
@@ -43,6 +44,7 @@ export class BidRejectCommand extends BaseCommand implements RpcCommandInterface
     public async execute( @request(RpcRequest) data: RpcRequest): Promise<SmsgSendResponse> {
 
         const bidId = data.params[0];
+        const reason = BidRejectReason[data.params[1]];
         const bid: resources.Bid = await this.bidService.findOne(bidId)
             .then(value => {
                 return value.toJSON();
@@ -54,30 +56,45 @@ export class BidRejectCommand extends BaseCommand implements RpcCommandInterface
     /**
      * data.params[]:
      * [0]: bidId
-     *
+     * [1]: reasonId
      * @param {RpcRequest} data
      * @returns {Promise<RpcRequest>}
      */
     public async validate(data: RpcRequest): Promise<RpcRequest> {
 
         if (data.params.length < 1) {
+            this.log.error('Missing bidId.');
             throw new MessageException('Missing bidId.');
         }
 
         if (typeof data.params[0] !== 'number') {
+            this.log.error('bidId should be a number.');
             throw new MessageException('bidId should be a number.');
         }
 
+        if (data.params.length >= 2) {
+            const reason = data.params[1];
+            if (typeof reason !== 'string') {
+                this.log.error('reasonEnum should be a string.');
+                throw new MessageException('reasonEnum should be a string.');
+            } else if (!BidRejectReason[reason]) {
+                this.log.error('reasonEnum should be a string with one of these values {OUT_OF_STOCK}.');
+                throw new MessageException('reasonEnum should be a string with one of these values {OUT_OF_STOCK}.');
+            }
+        }
+
         const bidId = data.params[0];
-        const bid: resources.Bid = await this.bidService.findOne(bidId)
-            .then(value => {
-                return value.toJSON();
-            });
+        let bid: any = await this.bidService.findOne(bidId);
+        if (!bid) {
+            this.log.error('Bid with specified bidId not found.');
+            throw new MessageException('Bid with specified bidId not found.');
+        }
+        bid = bid.toJSON();
 
         // make sure ListingItem exists
         if (_.isEmpty(bid.ListingItem)) {
-            this.log.error('ListingItem not found.');
-            throw new MessageException('ListingItem not found.');
+            this.log.error('ListingItem not found in bid.');
+            throw new MessageException('ListingItem not found in bid.');
         }
 
         // make sure we have a ListingItemTemplate, so we know it's our item
@@ -90,13 +107,14 @@ export class BidRejectCommand extends BaseCommand implements RpcCommandInterface
     }
 
     public usage(): string {
-        return this.getName() + ' <itemhash> <bidId> ';
+        return this.getName() + ' <bidId> <reasonId> ';
     }
 
     public help(): string {
         return this.usage() + ' -  ' + this.description() + '\n'
-        + '    <itemhash>               - String - The hash if the item whose bid we want to reject. '
-        + '    <bidId>                  - Numeric - The ID of the bid we want to reject. ';
+        // + '    <itemhash>               - String - The hash if the item whose bid we want to reject. '
+        + '    <bidId>                  - Numeric - The ID of the bid we want to reject. '
+        + '    <reasonId>               - [optional] Enum {OUT_OF_STOCK} - The predefined reason you want to specify for cancelling the bid. ';
     }
 
     public description(): string {
@@ -104,6 +122,6 @@ export class BidRejectCommand extends BaseCommand implements RpcCommandInterface
     }
 
     public example(): string {
-        return 'bid ' + this.getName() + ' b90cee25-036b-4dca-8b17-0187ff325dbb ';
+        return 'bid ' + this.getName() + ' b90cee25-036b-4dca-8b17-0187ff325dbb OUT_OF_STOCK ';
     }
 }
