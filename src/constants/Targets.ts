43f4e04fabfe13585b5e4bc41b3e61bda00b68ce
Juha Kovanen <juha.kovanen@gmail.com>
2019-06-30 02:49:58 +0300

    fixed conflicts

--- src/constants/Targets.ts
+++ src/constants/Targets.ts
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 // Copyright (c) 2017-2019, The Particl Market developers
 // Distributed under the GPL software license, see the accompanying
 // file COPYING or https://github.com/particl/particl-market/blob/develop/LICENSE
