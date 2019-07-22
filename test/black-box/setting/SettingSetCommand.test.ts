643b3c8698c057eb02ba6c5886b1f78dfcd55026
Juha Kovanen <juha.kovanen@gmail.com>
2019-07-22 14:53:03 +0300

    fixed setting set test

--- test/black-box/setting/SettingSetCommand.test.ts
+++ test/black-box/setting/SettingSetCommand.test.ts
@@ -3,10 +3,13 @@
 // file COPYING or https://github.com/particl/particl-market/blob/develop/LICENSE
 
 import * from 'jest';
+import * as resources from 'resources';
 import { BlackBoxTestUtil } from '../lib/BlackBoxTestUtil';
 import { Commands } from '../../../src/api/commands/CommandEnumType';
 import { Logger as LoggerType } from '../../../src/core/Logger';
-import * as resources from 'resources';
+import {MissingParamException} from '../../../src/api/exceptions/MissingParamException';
+import {InvalidParamException} from '../../../src/api/exceptions/InvalidParamException';
+import {ModelNotFoundException} from '../../../src/api/exceptions/ModelNotFoundException';
 
 describe('SettingSetCommand', () => {
 
@@ -18,8 +21,8 @@ describe('SettingSetCommand', () => {
     const settingCommand = Commands.SETTING_ROOT.commandName;
     const settingSetCommand = Commands.SETTING_SET.commandName;
 
-    let defaultMarket: resources.Market;
-    let defaultProfile: resources.Profile;
+    let market: resources.Market;
+    let profile: resources.Profile;
 
     const testData = {
         key: 'key',
@@ -35,81 +38,113 @@ describe('SettingSetCommand', () => {
         await testUtil.cleanDb();
 
         // get default profile and market
-        defaultProfile = await testUtil.getDefaultProfile();
-        defaultMarket = await testUtil.getDefaultMarket();
+        profile = await testUtil.getDefaultProfile();
+        market = await testUtil.getDefaultMarket();
 
     });
 
-    test('Should fail to create Setting when missing profileId', async () => {
+    test('Should fail to set Setting because missing key', async () => {
         const res = await testUtil.rpc(settingCommand, [settingSetCommand]);
         res.expectJson();
         res.expectStatusCode(404);
-        expect(res.error.error.success).toBe(false);
-        expect(res.error.error.message).toBe(`Missing profileId.`);
+        expect(res.error.error.message).toBe(new MissingParamException('key').getMessage());
     });
 
-    test('Should fail to create Setting when missing key', async () => {
+    test('Should fail to set Setting because missing value', async () => {
         const res = await testUtil.rpc(settingCommand, [settingSetCommand,
-            defaultProfile.id
-        ]);
-        res.expectJson();
-        res.expectStatusCode(404);
-        expect(res.error.error.success).toBe(false);
-        expect(res.error.error.message).toBe(`Missing key.`);
-    });
-
-    test('Should fail to create Setting when missing value', async () => {
-        const res = await testUtil.rpc(settingCommand, [settingSetCommand,
-            defaultProfile.id,
             testData.key
         ]);
         res.expectJson();
         res.expectStatusCode(404);
-        expect(res.error.error.success).toBe(false);
-        expect(res.error.error.message).toBe(`Missing value.`);
+        expect(res.error.error.message).toBe(new MissingParamException('value').getMessage());
     });
 
-    test('Should create one Setting', async () => {
+    test('Should fail to set Setting because missing profileId', async () => {
         const res = await testUtil.rpc(settingCommand, [settingSetCommand,
-            defaultProfile.id,
             testData.key,
             testData.value
         ]);
         res.expectJson();
+        res.expectStatusCode(404);
+        expect(res.error.error.message).toBe(new MissingParamException('profileId').getMessage());
+    });
+
+    test('Should fail to set Setting because invalid key', async () => {
+        const res = await testUtil.rpc(settingCommand, [settingSetCommand,
+            0,
+            testData.value,
+            profile.id
+        ]);
+        res.expectJson();
+        res.expectStatusCode(400);
+        expect(res.error.error.message).toBe(new InvalidParamException('key', 'string').getMessage());
+    });
+
+    test('Should fail to set Setting because invalid value', async () => {
+        const res = await testUtil.rpc(settingCommand, [settingSetCommand,
+            testData.key,
+            0,
+            profile.id
+        ]);
+        res.expectJson();
+        res.expectStatusCode(400);
+        expect(res.error.error.message).toBe(new InvalidParamException('value', 'string').getMessage());
+    });
+
+    test('Should fail to set Setting because invalid profileId', async () => {
+        const res = await testUtil.rpc(settingCommand, [settingSetCommand,
+            testData.key,
+            testData.value,
+            'INVALID'
+        ]);
+        res.expectJson();
+        res.expectStatusCode(400);
+        expect(res.error.error.message).toBe(new InvalidParamException('profileId', 'number').getMessage());
+    });
+
+    test('Should fail to set Setting because missing Profile model', async () => {
+        const missingProfileId = 0;
+        const res = await testUtil.rpc(settingCommand, [settingSetCommand,
+            testData.key,
+            testData.value,
+            missingProfileId
+        ]);
+        res.expectJson();
+        res.expectStatusCode(404);
+        expect(res.error.error.message).toBe(new ModelNotFoundException('Profile').getMessage());
+    });
+
+    test('Should set a Setting', async () => {
+        const res = await testUtil.rpc(settingCommand, [settingSetCommand,
+            testData.key,
+            testData.value,
+            profile.id
+        ]);
+        res.expectJson();
         res.expectStatusCode(200);
+
         const result: resources.Setting = res.getBody()['result'];
         expect(result.Profile).toBeDefined();
-        expect(result.Profile.id).toBe(defaultProfile.id);
+        expect(result.Profile.id).toBe(profile.id);
         expect(result.key).toBe(testData.key);
         expect(result.value).toBe(testData.value);
     });
 
     test('Should update the previously created Setting', async () => {
         const res = await testUtil.rpc(settingCommand, [settingSetCommand,
-            defaultProfile.id,
             testDataUpdated.key,
-            testDataUpdated.value
+            testDataUpdated.value,
+            profile.id
         ]);
         res.expectJson();
         res.expectStatusCode(200);
-        //
+
         const result: resources.Setting = res.getBody()['result'];
         expect(result.Profile).toBeDefined();
-        expect(result.Profile.id).toBe(defaultProfile.id);
+        expect(result.Profile.id).toBe(profile.id);
         expect(result.key).toBe(testDataUpdated.key);
         expect(result.value).toBe(testDataUpdated.value);
     });
 
-    test('Should fail to update Setting using invalid profileId', async () => {
-        const invalidProfile = 0;
-        const res = await testUtil.rpc(settingCommand, [settingSetCommand,
-            invalidProfile,
-            testData.key,
-            testData.value
-        ]);
-        res.expectJson();
-        // res.expectStatusCode(404);
-        expect(res.error.error.success).toBe(false);
-        expect(res.error.error.message).toBe(`Profile not found.`);
-    });
+
 });
