158fd9c0eeb547fd15410e7709a9429d8f7e394e
Juha Kovanen <juha.kovanen@gmail.com>
2019-09-05 12:02:52 +0300

    when resending smsgmessage, skip setting MPA_LOCK order+orderitem statuses in case MPA_SHIP was previously received

--- src/api/services/action/EscrowLockActionService.ts
+++ src/api/services/action/EscrowLockActionService.ts
@@ -38,6 +38,7 @@ import { CoreRpcService } from '../CoreRpcService';
 import { ActionMessageObjects } from '../../enums/ActionMessageObjects';
 import { KVS } from 'omp-lib/dist/interfaces/common';
 import { ActionDirection } from '../../enums/ActionDirection';
+import * as _ from '@types/lodash';
 
 export class EscrowLockActionService extends BaseActionService {
 
@@ -189,8 +190,19 @@ export class EscrowLockActionService extends BaseActionService {
             .then(async value => {
                 const bid: resources.Bid = value.toJSON();
 
-                await this.orderItemService.updateStatus(bid.ParentBid.OrderItem.id, OrderItemStatus.ESCROW_LOCKED);
-                await this.orderService.updateStatus(bid.ParentBid.OrderItem.Order.id, OrderStatus.PROCESSING);
+                // mp@0.1.7: because of a bug in smsg, some messages might not have been received and 'smsg resend'-command was added to allow resending
+                // those smsgmessages to fix the buy flow. it was possible for buyers to not receive the MPA_LOCK, but receive the next MPA_SHIP which
+                // the seller sends after the first one. Now, if seller resends the MPA_LOCK after MPA_SHIP has been received, the Order and OrderItem
+                // statuses will be set to incorrect values and the buyer would be incorrectly again waiting for the MPA_SHIP message and wont be able
+                // to send the MPA_RELEASE message.
+
+                // to fix this, if Order or OrderItem statuses are set to OrderStatus.SHIPPING and OrderItemStatus.SHIPPING -> dont update
+                // ...in other words, update if statuses are something else than SHIPPING
+                if (bid.ParentBid.OrderItem.status !== OrderItemStatus.SHIPPING
+                    && bid.ParentBid.OrderItem.Order.status !== OrderStatus.SHIPPING) {
+                    await this.orderItemService.updateStatus(bid.ParentBid.OrderItem.id, OrderItemStatus.ESCROW_LOCKED);
+                    await this.orderService.updateStatus(bid.ParentBid.OrderItem.Order.id, OrderStatus.PROCESSING);
+                }
 
                 return await this.bidService.findOne(bid.id, true).then(bidModel => bidModel.toJSON());
             });
