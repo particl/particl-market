9c099f44a1840ad366d4f4c7f8857f204b90490b
zaSmilingIdiot <30551211+zaSmilingIdiot@users.noreply.github.com>
2020-04-20 10:44:07 +0200

    Removes unnecessary additional queue library, to use the already included pm-queue.

--- src/core/ZmqWorker.ts
+++ src/core/ZmqWorker.ts
@@ -8,6 +8,8 @@ import { EventEmitter } from './api/events';
 import { IoC } from './IoC';
 import * as ParticlZmq from 'particl-zmq';
 import { CoreMessageProcessor } from '../api/messageprocessors/CoreMessageProcessor';
+import PQueue, { DefaultAddOptions, Options } from 'pm-queue';
+import PriorityQueue, { PriorityQueueOptions } from 'pm-queue/dist/priority-queue';
 import * as Queue from '@ronomon/queue';
 
 export class ZmqWorker {
@@ -18,10 +20,22 @@ export class ZmqWorker {
     private eventEmitter: EventEmitter;
     private coreMessageProcessor: CoreMessageProcessor;
     private isConnected = false;
-    private queue;
-    private queueWorkerCount = 5;
+    private actionQueue: PQueue;
 
     constructor(ioc: IoC) {
+        const queueOptions = {
+            concurrency: 5,             // concurrency limit
+            autoStart: true,            // auto-execute tasks as soon as they're added
+            throwOnTimeout: false       // throw on timeout
+        } as Options<PriorityQueue, PriorityQueueOptions>;
+
+        this.actionQueue = new PQueue(queueOptions);
+        this.actionQueue
+            .on('active', () => {
+                // emitted as each item is processed in the queue for the purpose of tracking progress.
+                this.log.debug(`ZMQWORKER QUEUE: queue size: ${this.actionQueue.size}, tasks pending: ${this.actionQueue.pending}`);
+            })
+            .start();
         this.eventEmitter = ioc.container.getNamed<EventEmitter>(Types.Core, Core.Events);
         this.eventEmitter.setMaxListeners(10);
         this.coreMessageProcessor = ioc.container.getNamed<CoreMessageProcessor>(Types.MessageProcessor, Targets.MessageProcessor.CoreMessageProcessor);
@@ -57,9 +71,9 @@ export class ZmqWorker {
         particld.on('smsg', async (msgid) => {
             this.log.debug('ZMQ: receive(smsg): ', msgid.toString('hex'));
             msgid = msgid.toString('hex').slice(4); // 4 first ones are the msg version
-            if (this.queue !== undefined) {
-                this.queue.push(msgid);
-            }
+            await this.actionQueue.add(async () =>
+                await this.coreMessageProcessor.process(msgid).catch(() => this.log.error('Failed to process msgid: ', + msgid))
+            );
         });
 
         particld.on('hashblock', (hash) => {
@@ -79,15 +93,11 @@ export class ZmqWorker {
         });
 
         particld.on('connect:*', (uri, type) => {
-            this.queue = new Queue(this.queueWorkerCount);
-            this.queue.onData = async (msgid, end) => {
-                await this.coreMessageProcessor.process(msgid)
-                    .catch(() => this.log.error('Failed to process msgid: ', + msgid))
-                    .then(() => end());
-            };
-
             this.isConnected = true;
             this.log.debug('ZMQ: connect:* ' + type + ', uri: ' + uri);
+            if (this.actionQueue.isPaused) {
+                this.actionQueue.start();
+            }
         });
 
         particld.on('close:*', (err, type) => {
@@ -95,8 +105,7 @@ export class ZmqWorker {
                 this.log.debug('ZMQ: close:* ' + type + ', error: ' + err);
             }
             this.isConnected = false;
-            this.queue.stop();
-            this.queue = undefined;
+            this.actionQueue.pause();
         });
 
         particld.on('retry:*', (type, attempt) => {
