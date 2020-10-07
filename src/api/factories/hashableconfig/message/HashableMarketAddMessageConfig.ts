191ecc790fc3058c0f7c381915ef95571df29b11
Juha Kovanen <juha.kovanen@gmail.com>
2020-10-07 15:11:15 +0300

    fixed market post market.hash generation

--- src/api/factories/hashableconfig/message/HashableMarketAddMessageConfig.ts
+++ src/api/factories/hashableconfig/message/HashableMarketAddMessageConfig.ts
@@ -16,18 +16,19 @@ export class HashableMarketAddMessageConfig extends BaseHashableConfig {
     }, {
         from: HashableMarketField.MARKET_DESCRIPTION,
         to: HashableMarketField.MARKET_DESCRIPTION
-    },
-    /* {
+    }
+    /*
+     MARKET_RECEIVE_ADDRESS and MARKET_PUBLISH_ADDRESS should be passed to the hasher
+     {
         from: HashableMarketAddField.MARKET_TYPE,       // marketType -> type
         to: HashableMarketField.MARKET_TYPE
-    },*/
-    {
+    }, {
         from: HashableMarketField.MARKET_RECEIVE_ADDRESS,
         to: HashableMarketField.MARKET_RECEIVE_ADDRESS
     }, {
         from: HashableMarketField.MARKET_PUBLISH_ADDRESS,
         to: HashableMarketField.MARKET_PUBLISH_ADDRESS
-    }/*, {
+    }, {
         from: HashableMarketAddField.MARKET_IMAGE_HASH,
         to: HashableMarketField.MARKET_IMAGE_HASH
     }*/
